4bd695039c4fe02ef55e4049ed14020751661a94
yuki sawabe <sawabe0v0@gmail.com>
2021-03-07 15:53:26 +0900

    rubocop -a

--- db/migrate/20210307025822_create_answers.rb
+++ db/migrate/20210307025822_create_answers.rb
@@ -1,10 +1,10 @@
 class CreateAnswers < ActiveRecord::Migration[6.1]
   def change
     create_table :answers do |t|
-      t.string :answer_1
-      t.string :answer_2
-      t.string :answer_3
-      t.string :answer_4
+      t.string :answer1
+      t.string :answer2
+      t.string :answer3
+      t.string :answer4
       t.integer :correct_answer_no
       t.string :commentary
       t.references :quiz, null: false, foreign_key: true
