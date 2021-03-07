582256d1806056317ca3e9861568ddf4eae9e034
yuki sawabe <sawabe0v0@gmail.com>
2021-03-07 16:08:17 +0900

    add validation for quiz, category and answer

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,9 @@
 class Answer < ApplicationRecord
   belongs_to :quiz
+  validates :answer1, presence: true
+  validates :answer2, presence: true
+  validates :answer3, presence: true
+  validates :answer4, presence: true
+  validates :correct_answer_no, presence: true
+  validates :commentary, presence: true
 end
--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,4 +1,5 @@
 class Quiz < ApplicationRecord
   has_one :category, dependent: :destroy
   has_one :answer, dependent: :destroy
+  validates :title, presence: true
 end
