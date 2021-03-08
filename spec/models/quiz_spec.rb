70585b46efda587c261754ee5b2a72468c3a6f73
yuki sawabe <sawabe0v0@gmail.com>
2021-03-08 18:21:06 +0900

    create unit test for quiz, answer and category

--- spec/models/quiz_spec.rb
+++ spec/models/quiz_spec.rb
@@ -1,5 +1,22 @@
 require "rails_helper"
 
 RSpec.describe Quiz, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "normal scenario" do
+    context "title were enterd" do
+      let(:quiz) { build(:quiz) }
+      it "Quiz is created" do
+        expect(quiz.valid?).to eq true
+      end
+    end
+  end
+
+  describe "exception scenario" do
+    context "title is not entered" do
+      let(:quiz) { build(:quiz, title: nil) }
+      it "error occurs" do
+        quiz.valid?
+        expect(quiz.errors.messages[:title]).to include "can't be blank"
+      end
+    end
+  end
 end
