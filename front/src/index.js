d2d9d0b5d85a339eaaa2bae6b5a814c5f69d0d53
yuki sawabe <sawabe0v0@gmail.com>
2021-06-19 21:10:12 +0900

    add history to index.js

--- front/src/index.js
+++ front/src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import { createStore } from "./reducks/store/store";
+import { ConnectedRouter } from "connected-react-router";
+import * as History from "history";
 
+const history = History.createBrowserHistory();
 export const store = createStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ConnectedRouter history={history}>
+      <App />
+    </ConnectedRouter>
   </Provider>,
   document.getElementById("root")
 );
