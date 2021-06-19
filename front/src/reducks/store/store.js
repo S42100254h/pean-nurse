bb79029ee6467b4f38938950610824dfff3d0618
yuki sawabe <sawabe0v0@gmail.com>
2021-06-19 21:03:21 +0900

    add connected-react-router

--- front/src/reducks/store/store.js
+++ front/src/reducks/store/store.js
@@ -1,14 +1,20 @@
 import {
   createStore as reduxCreateStore,
   combineReducers,
+  applyMiddleware,
 } from "redux";
+import { connectedRouter, routerMiddleware } from "connected-react-router";
 
 import { UsersReducer } from "../users/reducers";
 
-export const createStore = () => {
+export const createStore = (history) => {
   return reduxCreateStore(
     combineReducers({
+      router: connectedRouter(history),
       users: UsersReducer
-    })
+    }),
+    applyMiddleware(
+      routerMiddleware(history)
+    )
   );
 };
