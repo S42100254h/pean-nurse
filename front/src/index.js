4336d12e1c55bc704b1b9a9722629dd60e71b816
yuki sawabe <sawabe0v0@gmail.com>
2021-06-19 10:37:17 +0900

    connect store to redux

--- front/src/index.js
+++ front/src/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { Provider } from "react-redux";
+import createStore from "./reducks/store/store";
+
+export const store = createStore();
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById("root")
 );
