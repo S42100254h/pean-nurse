ecba4650f1780876de68c8e1437e8228212de136
yuki sawabe <sawabe0v0@gmail.com>
2021-06-23 23:22:25 +0900

    add demo data to SignIn component

--- front/src/templates/SignIn.jsx
+++ front/src/templates/SignIn.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useDispatch } from "react-redux";
 import { push } from "connected-react-router";
+import { signIn } from "../reducks/users/operations";
 
 const SignIn = () => {
   const dispatch = useDispatch();
@@ -8,7 +9,10 @@ const SignIn = () => {
   return (
     <div>
       <h2>サインイン</h2>
-      <button onClick={() => dispatch(push("/"))}>
+      <button onClick={() => {
+        dispatch(signIn("wanko@gmail.com", "a8810097"));
+        dispatch(push("/"));
+      }}>
         サインイン
       </button>
     </div>
