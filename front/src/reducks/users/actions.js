57e55be471990400310f8d15540851c955d9372d
yuki sawabe <sawabe0v0@gmail.com>
2021-06-26 16:58:35 +0900

    create SignUp actions

--- front/src/reducks/users/actions.js
+++ front/src/reducks/users/actions.js
@@ -1,3 +1,15 @@
+export const SIGN_UP = "SIGN_UP";
+export const signUpAction = (userState) => {
+  return {
+    type: "SIGN_UP",
+    payload: {
+      isSignedIn: true,
+      uid: userState.uid,
+      username: userState.username
+    },
+  };
+};
+
 export const SIGN_IN = "SIGN_IN";
 export const signInAction = (userState) => {
   return {
