38fd42b988eaa9586a6aab583066a3b9d322cefb
yuki sawabe <sawabe0v0@gmail.com>
2021-06-26 16:37:58 +0900

    update SignIn template

--- front/src/templates/SignIn.jsx
+++ front/src/templates/SignIn.jsx
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import { useDispatch } from "react-redux";
+import { TextInput, PrimaryButton } from "../components/UIkit";
 import { push } from "connected-react-router";
 import { signIn } from "../reducks/users/operations";
 
 const SignIn = () => {
   const dispatch = useDispatch();
 
+  const [ email, setEmail ] = useState(""),
+    [ password, setPassword ] = useState("");
+  
+  const inputEmail = useCallback((event) => {
+    setEmail(event.target.value);
+  }, [setEmail]);
+
+  const inputPassword = useCallback((event) => {
+    setPassword(event.target.value);
+  }, [setPassword]);
+
   return (
-    <div>
-      <h2>サインイン</h2>
-      <button onClick={() => {
-        dispatch(signIn("wanko@gmail.com", "a8810097"));
-        dispatch(push("/"));
-      }}>
-        サインイン
-      </button>
+    <div className="c-section-container">
+      <h2 className="u-text__headline u-text-center">サインイン</h2>
+      <div className="module-spacer--medium" />
+      <TextInput
+        fullWidth={true} label={"メールアドレス"} multiline={false} required={true} row={1} variant={"email"} onChange={inputEmail}
+      />
+      <TextInput
+        fullWidth={true} label={"パスワード"} multiline={false} required={true} row={1} variant={"password"} onChange={inputPassword}
+      />
+      <div className="module-spacer--medium" />
+      <PrimaryButton
+        label={"サインイン"}
+        onClick={() => {
+          dispatch(signIn(email, password));
+          dispatch(push("/"));
+        }}
+      />
     </div>
   );
 };
