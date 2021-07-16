b78e0b858252f573a188ea018be81358299cda95
yuki sawabe <sawabe0v0@gmail.com>
2021-07-16 12:38:41 +0900

    modify actions.js & test

--- front/src/reducks/users/actions.js
+++ front/src/reducks/users/actions.js
@@ -1,35 +1,22 @@
 export const SIGN_UP = "SIGN_UP";
-export const signUpAction = (userState) => {
+export const signUpAction = (payload) => {
   return {
     type: "SIGN_UP",
-    payload: {
-      isSignedIn: true,
-      uid: userState.uid,
-      name: userState.name
-    },
+    payload
   };
 };
 
 export const SIGN_IN = "SIGN_IN";
-export const signInAction = (userState) => {
+export const signInAction = (payload) => {
   return {
     type: "SIGN_IN",
-    payload: {
-      isSignedIn: true,
-      uid: userState.uid,
-      name: userState.name
-    },
+    payload,
   };
 };
 
 export const SIGN_OUT = "SIGN_OUT";
 export const signOutAction = () => {
   return {
-    type: "SIGN_OUT",
-    payload: {
-      isSignedIn: false,
-      uid: "",
-      name: ""
-    },
+    type: "SIGN_OUT"
   };
 };
