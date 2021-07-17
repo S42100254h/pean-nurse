8fe01e1bf0035d2edb25f5ed07b9188542e2a4d1
yuki sawabe <sawabe0v0@gmail.com>
2021-07-17 11:10:26 +0900

    create selectors for loading

---
+++ front/src/reducks/loading/selectors.js
@@ -0,0 +1,14 @@
+import { create } from "jss";
+import { createSelector } from "reselect";
+
+const loadingSelector = (state) => state.loading;
+
+export const getLoadingState = createSelector(
+  [loadingSelector],
+  state = state.state
+);
+
+export const getLoadingText = createSelector(
+  [loadingSelector],
+  state = state.text
+);
