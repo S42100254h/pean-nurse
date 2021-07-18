0d3a8e258d42f31915395ae632305c96ba82e5f2
yuki sawabe <sawabe0v0@gmail.com>
2021-07-18 10:57:19 +0900

    create Notification component

---
+++ front/src/components/Notification/Notification.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Snackbar from "@material-ui/core/Snackbar";
+import { makeStyles } from "@material-ui/core";
+import { useDispatch, useSelector } from "react-redux";
+import { closeNotificationAction } from "../../reducks/notification/actions";
+import { getNotificationIsOpen, getNotificationVariant, getNotificationMessage } from "../../reducks/notification/selectors";
+import { NotificationSnackbar } from "./index";
+
+const useStyles = makeStyles({
+  Snackbar: {
+    height: 80,
+  },
+});
+
+const Notification = () => {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const selector = useSelector((state) => state);
+  const isOpen = getNotificationIsOpen(selector);
+  const variant = getNotificationVariant(selector);
+  const message = getNotificationMessage(selector);
+  
+  return (
+    <Snackbar
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isOpen}
+      autoHideDuration={3000}
+      onClose={() => dispatch(closeNotificationAction())}
+      className={classes.Snackbar}
+    >
+      <NotificationSnackbar onClose={() => dispatch(closeNotificationAction())} variant={variant} message={message} />
+    </Snackbar>
+  );
+};
+
+export default Notification;
