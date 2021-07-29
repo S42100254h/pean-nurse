d74d2ea9319e38dd10e829c5e70859fb19a04c48
yuki sawabe <sawabe0v0@gmail.com>
2021-07-29 19:44:02 +0900

    add DashBoard to index

--- front/src/templates/index.js
+++ front/src/templates/index.js
@@ -1,3 +1,4 @@
+export { default as DashBoard } from "./DashBoard";
 export { default as Home } from "./Home";
 export { default as SignUp } from "./SignUp";
 export { default as SignIn } from "./SignIn";
