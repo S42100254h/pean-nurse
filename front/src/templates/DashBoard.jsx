352a50a7ac0ed333a4598f7fc693f5ea5b748a15
yuki sawabe <sawabe0v0@gmail.com>
2021-07-31 18:37:50 +0900

    modify location of Profile

--- front/src/templates/DashBoard.jsx
+++ front/src/templates/DashBoard.jsx
@@ -1,9 +1,20 @@
 import React from "react";
 import { Profile } from "../components/UIkit";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  profile: {
+    marginTop: "20px",
+  },
+}));
 
 const DashBoard = () => {
+  const classes = useStyles();
+
   return (
-    <Profile />
+    <div className={classes.profile}>
+      <Profile />
+    </div>
   );
 };
 
