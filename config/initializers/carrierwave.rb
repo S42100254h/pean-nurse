ef063fa318951a71ef9ae4851a96ec088f919ced
yuki sawabe <sawabe0v0@gmail.com>
2021-08-18 17:47:01 +0900

    enable to access S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,6 +6,7 @@ CarrierWave.configure do |config|
   config.storage :fog
   config.fog_provider = "fog/aws"
   config.fog_directory = ENV["AWS_S3_BUCKET_NAME"]
+  config.fog_public = false
   config.asset_host = "https://" + ENV["AWS_S3_BUCKET_NAME"] + ".s3-ap-northeast-1.amazonaws.com"
   config.fog_credentials = {
     provider: "AWS",
