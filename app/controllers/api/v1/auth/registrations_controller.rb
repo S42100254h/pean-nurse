86c5880ae6f06b1b9fad0a7ec51f1b863e606ed7
yuki sawabe <sawabe0v0@gmail.com>
2021-08-18 17:47:32 +0900

    add strong parameter

--- app/controllers/api/v1/auth/registrations_controller.rb
+++ app/controllers/api/v1/auth/registrations_controller.rb
@@ -2,10 +2,10 @@ class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsCon
   private
 
     def sign_up_params
-      params.permit(:name, :email, :password, :password_confirmation)
+      params.permit(:name, :email, :image, :password, :password_confirmation)
     end
 
     def account_update_params
-      params.permit(:name, :email)
+      params.permit(:name, :email, :image, :password, :password_confirmation)
     end
 end
