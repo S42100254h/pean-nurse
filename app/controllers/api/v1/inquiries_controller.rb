908323087d4db22e2c905af9a70e972894f1ebd4
yuki sawabe <sawabe0v0@gmail.com>
2021-09-01 13:06:53 +0900

    setup inquiries_controller

--- app/controllers/api/v1/inquiries_controller.rb
+++ app/controllers/api/v1/inquiries_controller.rb
@@ -1,4 +1,14 @@
 class Api::V1::InquiriesController < ApplicationController
   def create
+    @inquiry = Inquiry.new(inquiry_params)
+
+    if @inquiry.save
+      InquiryMailer.send_mail(@inquiry).deliver
+    end
   end
+  
+  private
+    def inquiry_params
+      params.require(:inquiry).permit(:email, :select, :text, :image)
+    end
 end
