300616e90a7a4497d4f2c907831d78f677b0ecc2
yuki sawabe <sawabe0v0@gmail.com>
2021-09-01 14:12:01 +0900

    modify routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'inquiries/create'
+      post 'inquiries/create'
     end
   end
   namespace "api", format: "json" do
