a3fc1e11c1613f32e58f4a9ab6fb02e29bb02600
yuki sawabe <sawabe0v0@gmail.com>
2021-09-01 11:09:43 +0900

    setup inquiry_mailer.rb

--- app/mailers/inquiry_mailer.rb
+++ app/mailers/inquiry_mailer.rb
@@ -1,2 +1,11 @@
 class InquiryMailer < ApplicationMailer
+  default from: "nursestepinquiry@gmail.com"
+
+  def send_mail(inquiry)
+    @inquiry = inquiry
+    mail(
+      to: "sawabe0v0@gmail.com",
+      subject: "【要確認】お問い合わせメール"
+    )
+  end
 end
