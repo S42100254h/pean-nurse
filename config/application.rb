fcc2e1f6eeb7455297415cc0153807b16de73729
yuki sawabe <sawabe0v0@gmail.com>
2021-09-06 08:20:51 +0900

    add Dotenv::Railtie.load to application.rb for loading environmental valuables

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+Dotenv::Railtie.load
+
 module NurseStep
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
