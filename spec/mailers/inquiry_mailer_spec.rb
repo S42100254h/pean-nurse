7f25b4ddcd8c162aa8a2352e851f4a52ef3b6e9c
yuki sawabe <sawabe0v0@gmail.com>
2021-09-09 07:10:15 +0900

    rubocop -A

--- spec/mailers/inquiry_mailer_spec.rb
+++ spec/mailers/inquiry_mailer_spec.rb
@@ -1,24 +1,23 @@
 require "rails_helper"
 
 RSpec.describe InquiryMailer, type: :mailer do
-
   describe "send_mail" do
-    let(:inquiry) { build(:inquiry) }
-
     subject(:mail) do
       @inquiry = inquiry
       @file = File.join(Rails.root, "front", "public", "cat.png")
       @filename = "cat.png"
 
-      described_class.send_mail(@inquiry, @file, @filename).deliver
+      InquiryMailer.send_mail(@inquiry, @file, @filename).deliver
       ActionMailer::Base.deliveries.last
     end
-    
+
+    let(:inquiry) { build(:inquiry) }
+
     it "when send_mail" do
       expect(mail.to.first).to eq ENV["TO_MAIL_ADDRESS"]
       expect(mail.from.first).to eq ENV["FROM_MAIL_ADDRESS"]
       expect(mail.subject).to eq "【要確認】お問い合わせメール"
-      
+
       # test for attachments
       expect(mail.attachments[0]).to be_a_kind_of(Mail::Part)
       expect(mail.attachments[0].content_type).to be_start_with("image/png")
