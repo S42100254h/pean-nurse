611d377f2aeceb5c5de9f3dabb1dce26f31d9617
yuki sawabe <sawabe0v0@gmail.com>
2021-09-17 08:19:40 +0900

    update layout of SignUp template

--- front/src/templates/SignUp.jsx
+++ front/src/templates/SignUp.jsx
@@ -1,10 +1,36 @@
 import React, { useCallback, useState} from "react";
+import { makeStyles } from "@material-ui/core";
 import { TextInput, PrimaryButton } from "../components/UIkit";
 import { useDispatch } from "react-redux";
 import { push } from "connected-react-router";
 import { signUp } from "../reducks/users/operations";
 
+const useStyles = makeStyles({
+  "container": {
+    margin: "0 auto",
+    maxWidth: 400,
+    padding: "1rem",
+    height: "auto",
+    width: "calc(100% - 2rem)",
+  },
+  "headline": {
+    color: "#4dd0e1",
+    fontSize: "1.563rem",
+    margin: "0 auto 1rem auto",
+    textAlign: "center",
+  },
+  "link": {
+    color: "#4dd0e1",
+    textAlign: "center",
+    textDecoration: "underline #4dd0e1",
+    "&:hover": {
+      cursor: "pointer",
+    },
+  },
+});
+
 const SignUp = () => {
+  const classes = useStyles();
   const dispatch = useDispatch();
 
   const [ name, setName ] = useState(""),
@@ -35,9 +61,9 @@ const SignUp = () => {
   };
 
   return (
-    <div className="c-section-container">
-      <h2 className="u-text__headline u-text-center">アカウント登録</h2>
-      <div className="module-spacer--medium" />
+    <div className={classes.container}>
+      <h2 className={classes.headline}>アカウント登録</h2>
+      <div className="module-spacer--extra-small" />
       <TextInput
         fullWidth={true} label={"ユーザー名"} multiline={false} required={true} row={1} value={name} type={"text"} onChange={inputName} onKeyDown={handleOnKeyDown}
       />
@@ -59,7 +85,8 @@ const SignUp = () => {
           dispatch(signUp(name, email, password, confirmPassword));
         }}
       />
-      <p onClick={() => dispatch(push("/signin"))}>アカウントをお持ちの方はこちら</p>
+      <div className="module-spacer--small" />
+      <p className={classes.link} onClick={() => dispatch(push("/signin"))}>アカウントをお持ちの方はこちら</p>
     </div>
   );
 };
