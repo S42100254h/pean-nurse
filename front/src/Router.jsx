574689f5ffd3a327672a6f8d2d63230f391d983e
yuki sawabe <sawabe0v0@gmail.com>
2021-09-22 07:41:08 +0900

    add PrivateRoute and UnAuthRoute to Router.jsx

--- front/src/Router.jsx
+++ front/src/Router.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import { Route, Switch } from "react-router";
 import { Deactivate,DashBoard, Home, Setting, SignUp, SignIn } from "./templates";
-import Auth from "./Auth";
+import PrivateRoute from "./PrivateRoute";
+import UnAuthRoute from "./UnAuthRoute";
 
 const Router = () => {
   return (
     <Switch>
-      <Route exact path={"(/)?"} component={Home} />
-      <Route exact path={"/signup"} component={SignUp} />
-      <Route exact path={"/signin"} component={SignIn} />
-      <Auth>
-        <Route exact path={"/dashboard"} component={DashBoard} />
-        <Route exact path={"/setting"} component={Setting} />
-        <Route exact path={"/deactivate"} component={Deactivate} />
-      </Auth>
+      <UnAuthRoute exact path={"(/)?"} component={Home} />
+      <UnAuthRoute exact path={"/signup"} component={SignUp} />
+      <UnAuthRoute exact path={"/signin"} component={SignIn} />
+      <PrivateRoute exact path={"/dashboard"} component={DashBoard} />
+      <PrivateRoute exact path={"/setting"} component={Setting} />
+      <PrivateRoute exact path={"/deactivate"} component={Deactivate} />
     </Switch>
   );
 };
