624a1c6f07042c78695fca4f7a3363cd96c5c2dc
yuki sawabe <sawabe0v0@gmail.com>
2021-09-27 17:15:18 +0900

    create PageNotFound component

---
+++ front/src/components/PageNotFound/PageNotFound.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PageNotFound = () => {
+  return (
+    <div>
+      <h1>404</h1>
+      <p>Page Not Found</p>
+      <h2>指定されたページが存在しません。</h2>
+    </div>
+  );
+};
+
+export default PageNotFound;
