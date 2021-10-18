278cdfd315006e72e562b4549dbad91bdb141d3e
yuki sawabe <sawabe0v0@gmail.com>
2021-10-18 11:55:59 +0900

    create test for put /api/v1/auth/password

--- spec/requests/api/v1/auth/passwords_spec.rb
+++ spec/requests/api/v1/auth/passwords_spec.rb
@@ -42,4 +42,22 @@ RSpec.describe "Api::V1::Auth::Passwords", type: :request do
       expect(response).to have_http_status(302)
     end
   end
+  
+  describe "PUT /api/v1/auth/password" do
+    subject { put(api_v1_user_password_path, headers: headers, params: params) }
+
+    context "send correct password & password_confirmation" do
+      let(:params) { { current_password: user.password, password: "dummypassword", password_confirmation: "dummypassword" } }
+      let(:headers) { user.create_new_auth_token }
+      let!(:user) { create(:user) }
+
+      it "reset password" do
+        subject
+        # http status 200 だが、パスワードの変更ができない。修正が必要。
+        # expect { subject }.to change { user.reload.password }.from(user.password).to(params[:password]) &
+                              # change { user.reload.password_confirmation }.from(user.password_confirmation).to(params[:password_confirmation])
+        expect(response).to have_http_status(200)
+      end
+    end
+  end
 end
