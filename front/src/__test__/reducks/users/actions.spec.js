7ca172c8089712cd7928a69790ec9619fe6e1598
yuki sawabe <sawabe0v0@gmail.com>
2021-10-18 15:20:55 +0900

    update test for reducks/users/actions.js

--- front/src/__test__/reducks/users/actions.spec.js
+++ front/src/__test__/reducks/users/actions.spec.js
@@ -2,14 +2,22 @@ import {
   SIGN_UP,
   SIGN_IN,
   SIGN_OUT,
+  EDIT_USER_INFO,
+  EDIT_USER_IMAGE,
+  DELETE_USER_IMAGE,
+  EDIT_USER_PASSWORD,
   signUpAction,
   signInAction,
   signOutAction,
+  editUserInfoAction,
+  editUserImageAction,
+  deleteUserImageAction,
+  editUserPasswordAction,
 } from "../../../reducks/users/actions";
 
 describe("actions.jsのテスト", () => {
   it("signUpActionのテスト", () => {
-    const dummy = { uid: "neko@gmail.com", name: "neko" };
+    const dummy = { name: "dummy", email: "dummy@gmail.com", password: "dummypassword", password_confirmation: "dummypassword" };
     const action = signUpAction(dummy);
 
     expect(action).toStrictEqual(
@@ -19,11 +27,9 @@ describe("actions.jsのテスト", () => {
       }
     );
   });
-});
 
-describe("actions.jsのテスト", () => {
   it("signInActionのテスト", () => {
-    const dummy = { uid: "neko@gmail.com", name: "neko" };
+    const dummy = { email: "dummy@gmail.com", password: "dummypassword" };
     const action = signInAction(dummy);
 
     expect(action).toStrictEqual(
@@ -33,9 +39,7 @@ describe("actions.jsのテスト", () => {
       }
     );
   });
-});
 
-describe("actions.jsのテスト", () => {
   it("signOutActionのテスト", () => {
     const action = signOutAction();
 
@@ -44,5 +48,51 @@ describe("actions.jsのテスト", () => {
         type: SIGN_OUT
       }
     );
+  });
+  
+  it("editUserInfoActionのテスト", () => {
+    const dummy = { name: "dummy", email:"dummy@gmail.com" };
+    const action = editUserInfoAction(dummy);
+    
+    expect(action).toStrictEqual(
+      {
+        type: EDIT_USER_INFO,
+        payload: dummy
+      }
+    )
+  });
+  
+  it("editUserImageActionのテスト", () => {
+    const dummy = { image: "dummy.png" };
+    const action = editUserImageAction(dummy);
+
+    expect(action).toStrictEqual(
+      {
+        type: EDIT_USER_IMAGE,
+        payload: dummy
+      }
+    )
+  });
+
+  it("deleteUserImageActionのテスト", () => {
+    const action = deleteUserImageAction();
+
+    expect(action).toStrictEqual(
+      {
+        type: DELETE_USER_IMAGE
+      }
+    )
+  });
+  
+  it("editUserPasswordActionのテスト", () => {
+    const dummy = { current_password: "currentpassword", password: "newpassword", password_confirmation: "newpassword" };
+    const action = editUserPasswordAction(dummy);
+
+    expect(action).toStrictEqual(
+      {
+        type: EDIT_USER_PASSWORD,
+        payload: dummy
+      }
+    )
   });
 });
