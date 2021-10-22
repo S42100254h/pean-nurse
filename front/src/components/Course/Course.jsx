a8e15cc3f7bfe672ec15f7b4c364dfed4c745b86
yuki sawabe <sawabe0v0@gmail.com>
2021-10-22 13:26:08 +0900

    create Course component

---
+++ front/src/components/Course/Course.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core";
+import { Box } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  container: {
+    width: 200,
+    height: 250,
+    backgroundColor: "#fff",
+    boxShadow: "0 0 1px grey",
+    textAlign: "center",
+  },
+  title: {
+    fontSize: 20,
+    height: "auto",
+    padding: "10px 0px",
+  },
+  image: {
+
+  },
+  caption: {
+    padding: "10px 0px",
+    fontSize: 12,
+  },
+  number: {
+    padding: "10px 0px",
+  },
+});
+
+const Course = (props) => {
+  const classes = useStyles();
+
+  return (
+    <Box
+      onClick={props.onClick}
+      className={classes.container}
+    >
+      <div className={classes.title}>{props.title}</div>
+      <img src={props.image} width="100px" height="100px" />
+      <div className={classes.caption}>{props.caption}</div>
+      <div>全{props.number}コース</div>
+    </Box>
+  );
+};
+
+export default Course;
