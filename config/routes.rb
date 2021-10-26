081e33765698905be4fb0d08ff945a16c96e7999
yuki sawabe <sawabe0v0@gmail.com>
2021-10-27 07:14:04 +0900

    modify routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,10 @@ Rails.application.routes.draw do
     namespace "v1" do
       mount_devise_token_auth_for "User", at: "auth", controllers: {
 
-  mount_devise_token_auth_for 'Admin', at: 'auth'
-  as :admin do
-    # Define routes for Admin within this block.
-  end
         registrations: "api/v1/auth/registrations",
       }
       get "users/currentuser"
+      mount_devise_token_auth_for 'Admin', at: 'admin'
     end
   end
 
