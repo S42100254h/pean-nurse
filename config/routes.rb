d567cedd1457ef0a9ddba515fe4c3166a7212d51
yuki sawabe <sawabe0v0@gmail.com>
2021-10-27 07:09:35 +0900

    rails generate devise_token_auth:install Admin

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
   namespace "api", format: "json" do
     namespace "v1" do
       mount_devise_token_auth_for "User", at: "auth", controllers: {
+
+  mount_devise_token_auth_for 'Admin', at: 'auth'
+  as :admin do
+    # Define routes for Admin within this block.
+  end
         registrations: "api/v1/auth/registrations",
       }
       get "users/currentuser"
