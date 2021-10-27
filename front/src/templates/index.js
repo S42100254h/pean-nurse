b53813007b732066fb8d0f5922691ec387c22223
yuki sawabe <sawabe0v0@gmail.com>
2021-10-27 17:00:27 +0900

    add AdminSignIn to index

--- front/src/templates/index.js
+++ front/src/templates/index.js
@@ -1,3 +1,4 @@
+export { default as AdminSignIn } from "./AdminSignIn";
 export { default as CourseList } from "./CourseList";
 export { default as Deactivate } from "./Deactivate";
 export { default as DashBoard } from "./DashBoard";
