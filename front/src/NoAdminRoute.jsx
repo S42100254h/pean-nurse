ecac14ec909f0af973b88e03ab98268a488bf4e0
yuki sawabe <sawabe0v0@gmail.com>
2021-10-28 19:19:01 +0900

    create NoAdminRoute component

---
+++ front/src/NoAdminRoute.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect } from "react";
+import { Route } from "react-router";
+import { useDispatch, useSelector } from "react-redux";
+import { getSignedIn } from "./reducks/admins/selectors";
+import { redirectToManagement } from "./reducks/admins/operations";
+
+const NoAdminRoute = ({ ...props }) => {
+  const dispatch = useDispatch();
+  const selector = useSelector((state) => state);
+  
+  const isSignedIn = getSignedIn(selector);
+  
+  useEffect(() => {
+    if (!isSignedIn) {
+      dispatch(redirectToManagement());
+    }
+  }, [])
+  
+  if (!isSignedIn) {
+    return <Route { ...props } />;
+  } else {
+    return <></>;
+  }
+};
+
+export default NoAdminRoute;
