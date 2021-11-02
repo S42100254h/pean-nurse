72a74c1a3a92f6c2fc86c902ca49438840d40345
yuki sawabe <sawabe0v0@gmail.com>
2021-11-02 16:57:37 +0900

    delete answers table

--- db/schema.rb
+++ db/schema.rb
@@ -37,19 +37,6 @@ ActiveRecord::Schema.define(version: 2021_10_26_220631) do
     t.index ["uid", "provider"], name: "index_admins_on_uid_and_provider", unique: true
   end
 
-  create_table "answers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "answer1"
-    t.string "answer2"
-    t.string "answer3"
-    t.string "answer4"
-    t.integer "correct_answer_no"
-    t.string "commentary"
-    t.bigint "quiz_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["quiz_id"], name: "index_answers_on_quiz_id"
-  end
-
   create_table "categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.bigint "quiz_id", null: false
@@ -99,6 +86,5 @@ ActiveRecord::Schema.define(version: 2021_10_26_220631) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
-  add_foreign_key "answers", "quizzes"
   add_foreign_key "categories", "quizzes"
 end
