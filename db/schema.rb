b4f8e4af733f72e33e9cc5f985092fc4f6d3787f
yuki sawabe <sawabe0v0@gmail.com>
2021-11-02 17:36:32 +0900

    delete answers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_02_082654) do
+ActiveRecord::Schema.define(version: 2021_11_02_083539) do
 
   create_table "admins", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -35,19 +35,6 @@ ActiveRecord::Schema.define(version: 2021_11_02_082654) do
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
     t.index ["uid", "provider"], name: "index_admins_on_uid_and_provider", unique: true
-  end
-
-  create_table "answers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "answer1"
-    t.string "answer2"
-    t.string "answer3"
-    t.string "answer4"
-    t.integer "correct_answer_no"
-    t.string "commentary"
-    t.bigint "quiz_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["quiz_id"], name: "index_answers_on_quiz_id"
   end
 
   create_table "categories", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -108,7 +95,6 @@ ActiveRecord::Schema.define(version: 2021_11_02_082654) do
     t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
   end
 
-  add_foreign_key "answers", "quizzes"
   add_foreign_key "categories", "quizzes"
   add_foreign_key "choices", "quizzes"
 end
