804698686f3e2608834373e83e70dedf4a32f27e
yuki sawabe <sawabe0v0@gmail.com>
2021-11-05 14:09:36 +0900

    fix factories about choices

--- spec/factories/choices.rb
+++ spec/factories/choices.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :choice do
-    choice { Faker::Lorem.questions(number: 1) }
+    choice { Faker::Lorem.word }
     is_right { Faker::Boolean.boolean }
     quiz
   end
