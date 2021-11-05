0cc64a60472caab7b504eb6a329281a9d7beafe2
yuki sawabe <sawabe0v0@gmail.com>
2021-11-05 13:20:34 +0900

    create choices controller

--- app/controllers/api/v1/choices_controller.rb
+++ app/controllers/api/v1/choices_controller.rb
@@ -1,16 +1,40 @@
-class Api::V1::ChoicesController < ApplicationController
-  def show
-  end
+class Api::V1::ChoicesController < Api::V1::ApiController
+  before_action :set_choice, only: [:update, :destroy]
+  before_action :authenticate_admin!, only: [:create, :update, :destroy]
 
   def index
+    choices = Choice.where("quiz_id": params[:quiz_id])
+    render json: choices
+  end
+
+  def show
+    choice = Choice.find(params[:id])
+    render json: choice
   end
 
   def create
+    quiz = Quiz.find(params[:choice][:quiz_id])
+    choice = quiz.choices.create!(choice_params)
+    render json: choice
   end
 
   def update
+    @choice.update!(choice_params)
+    render json: @choice
   end
 
   def destroy
+    @choice.destroy!
+    render json: @choice
   end
+
+  private
+
+    def set_choice
+      @choice = Choice.find(params[:id])
+    end
+
+    def choice_params
+      params.require(:choice).permit(:choice, :is_right, :quiz_id)
+    end
 end
