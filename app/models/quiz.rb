c7030f82aaedc5cca1db0f9602861bffaadb3aa6
yuki sawabe <sawabe0v0@gmail.com>
2021-11-05 09:15:14 +0900

    fix relation of quiz model

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,5 +1,5 @@
 class Quiz < ApplicationRecord
   has_one :category, dependent: :destroy
-  has_one :answer, dependent: :destroy
+  has_one :choice, dependent: :destroy
   validates :title, presence: true
 end
