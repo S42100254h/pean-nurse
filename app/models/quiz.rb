dd62ea6f4ef819324c4bc053d95f00f9c66db6d4
yuki sawabe <sawabe0v0@gmail.com>
2021-11-05 09:46:29 +0900

    add validation to Choice model

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -1,5 +1,5 @@
 class Quiz < ApplicationRecord
-  has_many :category, dependent: :destroy
-  has_many :choice, dependent: :destroy
+  has_many :categories, dependent: :destroy
+  has_many :choices, dependent: :destroy
   validates :title, presence: true
 end
