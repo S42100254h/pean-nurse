f50dd0d3e96144e824b39bc4f10dbcdd652a84d5
yuki sawabe <sawabe0v0@gmail.com>
2021-11-11 07:08:31 +0900

    modify ConfirmDialog component

--- front/src/components/ConfirmDialog/ConfirmDialog.jsx
+++ front/src/components/ConfirmDialog/ConfirmDialog.jsx
@@ -3,7 +3,7 @@ import { useDispatch } from "react-redux";
 import { makeStyles } from "@material-ui/core";
 import { Dialog, DialogContent } from "@material-ui/core";
 import { PrimaryButton } from "../UIkit";
-import { createQuiz } from "../../function/quiz";
+import { createQuiz, editQuiz } from "../../function/quiz";
 import { Choice, Quiz } from "./index";
 
 const useStyles = makeStyles({
@@ -19,7 +19,7 @@ const useStyles = makeStyles({
   },
 });
 
-const ConfirmDialog = ({ quiz, choice1, select1, choice2, select2, choice3, select3, choice4, select4, open, onClose }) => {
+const ConfirmDialog = ({ quiz, choice1, select1, choice2, select2, choice3, select3, choice4, select4, open, onClose, headline, buttonLabel, func }) => {
   const classes = useStyles();
   const dispatch = useDispatch();
   
@@ -33,7 +33,7 @@ const ConfirmDialog = ({ quiz, choice1, select1, choice2, select2, choice3, sele
       >
         <DialogContent>
           <div className={classes.container}>
-            <div className={classes.headline}>以下の内容でクイズを作成してもよろしいですか？</div>
+            <div className={classes.headline}>{headline}</div>
             <div className="module-spacer--extra-small" />
             <Quiz quiz={quiz} label={"問題"} />
             <Choice choice={choice1} select={select1} label={"選択肢１"} />
@@ -42,9 +42,9 @@ const ConfirmDialog = ({ quiz, choice1, select1, choice2, select2, choice3, sele
             <Choice choice={choice4} select={select4} label={"選択肢４"} />
             <div className="module-spacer--extra-extra-small" />
             <PrimaryButton
-              label={"クイズを作成する"}
+              label={buttonLabel}
               fullWidth={true}
-              onClick={() => dispatch(createQuiz(quiz, choice1, select1, choice2, select2, choice3, select3, choice4, select4 ))}
+              onClick={() => dispatch(func(quiz, choice1, select1, choice2, select2, choice3, select3, choice4, select4))}
             />
           </div>
         </DialogContent>
