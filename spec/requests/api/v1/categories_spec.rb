a35d62f2f898ebd835cee7db23becf3f09293c1a
yuki sawabe <sawabe0v0@gmail.com>
2021-11-14 08:19:55 +0900

    rubocop -a

--- spec/requests/api/v1/categories_spec.rb
+++ spec/requests/api/v1/categories_spec.rb
@@ -3,19 +3,19 @@ require "rails_helper"
 RSpec.describe "Api::V1::Categories", type: :request do
   describe "POST /api/v1/categories" do
     subject { post(api_v1_categories_path, params: params, headers: headers) }
-    
+
     context "send correct category information" do
       let(:params) { { category: attributes_for(:category) } }
       let(:current_admin) { create(:admin) }
       let(:headers) { current_admin.create_new_auth_token }
-      
+
       it "Category is created" do
         expect { subject }.to change { Category.count }.by(1)
         expect(response).to have_http_status(200)
       end
     end
   end
-  
+
   describe "GET /api/v1/categories" do
     subject { get(api_v1_categories_path) }
 
@@ -31,14 +31,14 @@ RSpec.describe "Api::V1::Categories", type: :request do
       expect(response).to have_http_status(200)
     end
   end
-  
+
   describe "GET /api/v1/categories/:id" do
     subject { get(api_v1_category_path(category_id)) }
 
     context "specified id exists" do
       let(:category) { create(:category) }
       let(:category_id) { category.id }
-      
+
       it "get detail of category" do
         subject
         res = JSON.parse(response.body)
@@ -48,7 +48,7 @@ RSpec.describe "Api::V1::Categories", type: :request do
       end
     end
   end
-  
+
   describe "PATCH /api/v1/categories/:id" do
     subject { patch(api_v1_category_path(category_id), params: params, headers: headers) }
 
@@ -57,22 +57,22 @@ RSpec.describe "Api::V1::Categories", type: :request do
     let(:params) { { category: { name: Faker::Lorem.word, created_at: Time.current } } }
     let(:category_id) { category.id }
     let(:category) { create(:category) }
-    
+
     it "category is updated" do
       expect { subject }.to change { category.reload.name }.from(category.name).to(params[:category][:name]) &
                             not_change { category.reload.created_at }
       expect(response).to have_http_status(200)
     end
   end
-  
+
   describe "DELETE /api/v1/categories/:id" do
     subject { delete(api_v1_category_path(category_id), headers: headers) }
-    
+
     let(:headers) { current_admin.create_new_auth_token }
     let(:current_admin) { create(:admin) }
     let(:category_id) { category.id }
     let!(:category) { create(:category) }
-    
+
     it "category is deleted" do
       expect { subject }.to change { Category.count }.by(-1)
       expect(response).to have_http_status(200)
