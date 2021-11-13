487566a21ee075e14dfe24c00eec37d91768b68b
yuki sawabe <sawabe0v0@gmail.com>
2021-11-13 09:48:43 +0900

    create test for DELETE /api/v1/admin/sign_out

--- spec/requests/api/v1/admin/sessions_spec.rb
+++ spec/requests/api/v1/admin/sessions_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe "Api::V1::Admin::Sessions", type: :request do
     context "send correct admin information" do
       let(:params) { { email: admin.email, password: admin.password } }
       let!(:admin) { create(:admin) }
-      
+
       it "Admin can sign in" do
         subject
         headers = response.headers
@@ -43,6 +43,24 @@ RSpec.describe "Api::V1::Admin::Sessions", type: :request do
         expect(headers["client"]).to be nil
         expect(headers["access-token"]).to be nil
         expect(response).to have_http_status(401)
+      end
+    end
+  end
+
+  describe "DELETE /api/v1/admin/sign_out" do
+    subject { delete(destroy_api_v1_admin_session_path, headers: headers) }
+
+    context "send correct information" do
+      let(:headers) { admin.create_new_auth_token }
+      let!(:admin) { create(:admin) }
+
+      it "Admincan sign out" do
+        subject
+        headers = response.headers
+        expect(headers["uid"]).to be nil
+        expect(headers["client"]).to be nil
+        expect(headers["access-token"]).to be nil
+        expect(response).to have_http_status(200)
       end
     end
   end
