77f8ea827c8eb0330b86429605753acd06800dca
yuki sawabe <sawabe0v0@gmail.com>
2021-11-13 21:00:13 +0900

    delete unnecessary migration files

--- db/migrate/20211113105710_change_column_to_category_quiz_reration.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnToCategoryQuizReration < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :category_quiz_relations
-  end
-end
--- db/migrate/20211113105850_change_column_to_category_quizzes.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnToCategoryQuizzes < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :category_quizzes
-  end
-end
