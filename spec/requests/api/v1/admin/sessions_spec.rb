7e5942355c51c2406d857d8ce300094beaf35c64
yuki sawabe <sawabe0v0@gmail.com>
2021-11-13 09:44:58 +0900

    create test for POST /api/v1/admin/sign_in

---
+++ spec/requests/api/v1/admin/sessions_spec.rb
@@ -0,0 +1,49 @@
+require "rails_helper"
+
+RSpec.describe "Api::V1::Admin::Sessions", type: :request do
+  describe "POST /api/v1/admin/sign_in" do
+    subject { post(api_v1_admin_session_path, params: params) }
+
+    context "send correct admin information" do
+      let(:params) { { email: admin.email, password: admin.password } }
+      let!(:admin) { create(:admin) }
+      
+      it "Admin can sign in" do
+        subject
+        headers = response.headers
+        expect(headers["uid"]).to be_present
+        expect(headers["client"]).to be_present
+        expect(headers["access-token"]).to be_present
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context "email was wrong" do
+      let(:params) { { email: Faker::Internet.email, password: admin.password } }
+      let!(:admin) { create(:admin) }
+
+      it "Admin cannot sign in" do
+        subject
+        headers = response.headers
+        expect(headers["uid"]).to be nil
+        expect(headers["client"]).to be nil
+        expect(headers["access-token"]).to be nil
+        expect(response).to have_http_status(401)
+      end
+    end
+
+    context "password was wrong" do
+      let(:params) { { email: admin.email, password: Faker::Internet.password } }
+      let!(:admin) { create(:admin) }
+
+      it "Admin cannot sign in" do
+        subject
+        headers = response.headers
+        expect(headers["uid"]).to be nil
+        expect(headers["client"]).to be nil
+        expect(headers["access-token"]).to be nil
+        expect(response).to have_http_status(401)
+      end
+    end
+  end
+end
