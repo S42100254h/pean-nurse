17a5e43936b0c49133db4d6726bbeb0746a6b6da
yuki sawabe <sawabe0v0@gmail.com>
2021-11-13 20:46:26 +0900

    create exception scenario test for category_quiz_relation

--- spec/models/category_quiz_relation_spec.rb
+++ spec/models/category_quiz_relation_spec.rb
@@ -6,8 +6,35 @@ RSpec.describe CategoryQuizRelation, type: :model do
       let(:category) { create(:category) }
       let(:quiz) { create(:quiz) }
       let!(:category_quiz_relation) { create(:category_quiz_relation, category: category, quiz: quiz) }
+
       it "category_quiz_relation is created" do
         expect(category_quiz_relation.valid?).to eq true
+      end
+    end
+  end
+  
+  describe "exception scenario" do
+    context "category_id is blank" do
+      let(:category) { create(:category) }
+      let(:quiz) { create(:quiz) }
+      let!(:category_quiz_relation) { create(:category_quiz_relation, category: category, quiz: quiz) }
+
+      it "error occurs" do
+        category_quiz_relation.category_id = nil
+        category_quiz_relation.valid?
+        expect(category_quiz_relation.errors.messages[:category]).to include "must exist"
+      end
+    end
+
+    context "quiz_id is blank" do
+      let(:category) { create(:category) }
+      let(:quiz) { create(:quiz) }
+      let!(:category_quiz_relation) { create(:category_quiz_relation, category: category, quiz: quiz) }
+
+      it "error occurs" do
+        category_quiz_relation.quiz_id = nil
+        category_quiz_relation.valid?
+        expect(category_quiz_relation.errors.messages[:quiz]).to include "must exist"
       end
     end
   end
