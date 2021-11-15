2542b4325fb1eafb8fb41cd1028a0001dcc1edb6
yuki sawabe <sawabe0v0@gmail.com>
2021-11-15 17:36:21 +0900

    add uniqueness validation to Category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,5 @@ class Category < ApplicationRecord
   has_many :category_quiz_relations, dependent: :destroy
   has_many :quizzes, through: :category_quiz_relations
   validates :name, presence: true
+  validates :name, uniqueness: true
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -18,5 +18,14 @@ RSpec.describe Category, type: :model do
         expect(category.errors.messages[:name]).to include "can't be blank"
       end
     end
+
+    context "same name has already existed" do
+      let(:category) { build(:category, name: "neko") }
+      let!(:old_category) { create(:category, name: "neko") }
+      it "error occurs" do
+        category.valid?
+        expect(category.errors.messages[:name]).to include "has already been taken"
+      end
+    end
   end
 end
