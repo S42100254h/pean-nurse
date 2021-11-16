cb9bed9dda3e42887bfd19596603b0e471de38b9
yuki sawabe <sawabe0v0@gmail.com>
2021-11-17 06:50:39 +0900

    rubocop -a

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :category do
-    sequence(:name) { |n| "#{n}_#{Faker::Lorem.word}" }
+    sequence(:name) {|n| "#{n}_#{Faker::Lorem.word}" }
   end
 end
--- spec/factories/quizzes.rb
+++ spec/factories/quizzes.rb
@@ -1,5 +1,5 @@
 FactoryBot.define do
   factory :quiz do
-    sequence(:title) { |n| "#{n}_#{Faker::Lorem.question}" }
+    sequence(:title) {|n| "#{n}_#{Faker::Lorem.question}" }
   end
 end
