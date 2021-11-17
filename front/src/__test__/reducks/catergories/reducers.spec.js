118b49120ffb4b0c4a6b6681d15735d481a7cb94
yuki sawabe <sawabe0v0@gmail.com>
2021-11-17 22:20:12 +0900

    create test for reducers.js

---
+++ front/src/__test__/reducks/catergories/reducers.spec.js
@@ -0,0 +1,33 @@
+import initialState from "../../../reducks/store/initialState";
+import { CategoriesReducer } from "../../../reducks/categories/reducers";
+import { fetchCategoriesAction, deleteCategoryAction } from "../../../reducks/categories/actions";
+
+describe("CategoriesReducerのテスト", () => {
+  const init = initialState.categories;
+
+  it("action.type === FETCTH_CATEGORIES", () => {
+    const dummy = [{ id: 1, name: "neko"}, { id: 2, name: "cat" }];
+    const action = fetchCategoriesAction(dummy);
+    const newState = CategoriesReducer(init, action);
+
+    expect(newState.list).toStrictEqual(
+      [
+        { id: 1, name: "neko"},
+        { id: 2, name: "cat"},
+      ]
+    );
+  });
+  
+  it("action.type === DELETE_CATEGORY", () => {
+    const init = [{ id: 1, name: "neko"}, { id: 2, name: "cat" }];
+    const dummy = [{ id:1, name: "neko" }];
+    const action = deleteCategoryAction(dummy);
+    const newState = CategoriesReducer(init, action);
+
+    expect(newState.list).toStrictEqual(
+      [
+        { id: 1, name: "neko" }
+      ]
+    );
+  });
+});
