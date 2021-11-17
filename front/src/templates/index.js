42b1c0bb44539bcefcbd8f7bf4499478adbef3b3
yuki sawabe <sawabe0v0@gmail.com>
2021-11-17 19:28:08 +0900

    add CategoryList to index

--- front/src/templates/index.js
+++ front/src/templates/index.js
@@ -1,4 +1,5 @@
 export { default as AdminSignIn } from "./AdminSignIn";
+export { default as CategoryList } from "./CategoryList";
 export { default as CourseOverview } from "./CourseOverview";
 export { default as CourseList } from "./CourseList";
 export { default as CreateCategory } from "./CreateCategory";
