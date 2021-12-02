79671dbf5af5d9f178b51183b365c6f09b59ca7d
yuki sawabe <sawabe0v0@gmail.com>
2021-12-02 17:24:38 +0900

    add file-loader to webpack.config.js

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -45,6 +45,10 @@ module.exports = {
         test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
+      {
+        test: /\.(jpg|png)$/,
+        loader: "file-loader",
+      },
     ],
   },
 };
