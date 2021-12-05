56a62fe31be84321f95ed2bd2badbb1356a787f3
yuki sawabe <sawabe0v0@gmail.com>
2021-12-05 13:43:37 +0900

    delete code about manifest.json

--- front/webpack.config.js
+++ front/webpack.config.js
@@ -2,7 +2,6 @@ const path = require("path");
 const Dotenv = require("dotenv-webpack");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const InterpolateHtmlPlugin = require("interpolate-html-plugin");
-const WebpackPwaManifest = require("webpack-pwa-manifest");
 const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 
 module.exports = {
@@ -18,12 +17,6 @@ module.exports = {
       title: "PeAN",
       template: "./public/index.html",
       favicons: "./src/assets/img/favicon.ico",
-    }),
-    new WebpackPwaManifest({
-      short_name: "PeAN",
-      name: "PeAN",
-      display: "standalone",
-      start_url: "index.html",
     }),
     new InterpolateHtmlPlugin({
       "PUBLIC_URL": "http://localhost:3000",
