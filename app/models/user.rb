46f2bd8d3dc3388f9c3779261196d3367b4020f5
yuki sawabe <sawabe0v0@gmail.com>
2021-12-06 17:09:59 +0900

    add "attr_accessor :current_password" to User model"

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :name, presence: true, length: { maximum: 50 }
   mount_uploader :image, ImageUploader
+  attr_accessor :current_password
   # devise_token_authにより、emailの形式とpasswordの長さは下記のように設定されている。
   # email_regexp = /\A[^@\s]+@[^@\s]+\z/
   # password_length = 6..128
