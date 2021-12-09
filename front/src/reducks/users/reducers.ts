7234c629ff5c554f8ca1dfd8555fc8d16111b745
yuki sawabe <sawabe0v0@gmail.com>
2021-12-09 10:12:32 +0900

    rewright actions & reducers about user with typescript-fsa

--- front/src/reducks/users/reducers.ts
+++ front/src/reducks/users/reducers.ts
@@ -1,47 +1,37 @@
+import { reducerWithInitialState } from "typescript-fsa-reducers";
 import initialState from "../store/initialState";
 import * as Actions from "./actions";
 
-export const UsersReducer = (state = initialState.users, action) => {
-  switch (action.type) {
-  case Actions.SIGN_IN:
-    return {
-      ...state,
-      isSignedIn: true,
-      uid: action.payload.uid,
-      name: action.payload.name,
-      email: action.payload.email,
-      image: action.payload.image,
-    };
-  case Actions.SIGN_UP:
-    return {
-      ...state,
-      isSignedIn: true,
-      uid: action.payload.uid,
-      name: action.payload.name,
-      email: action.payload.email,
-      image: action.payload.image,
-    };
-  case Actions.SIGN_OUT:
-    return {
-      ...initialState.users
-    };
-  case Actions.EDIT_USER_INFO:
-    return {
-      ...state,
-      name: action.payload.name,
-      email: action.payload.email,
-    };
-  case Actions.EDIT_USER_IMAGE:
-    return {
-      ...state,
-      image: action.payload.image,
-    };
-  case Actions.DELETE_USER_IMAGE:
-    return {
-      ...state,
-      image: "",
-    };
-  default:
-    return state;
-  }
-};
+export const UsersReducer = reducerWithInitialState(initialState.users)
+  .case(Actions.signUpAction, (state, payload) => ({
+    ...state,
+    isSignedIn: true,
+    uid: payload.uid,
+    name: payload.name,
+    email: payload.email,
+    image: payload.image,
+  }))
+  .case(Actions.signInAction, (state, payload) => ({
+    ...state,
+    isSignedIn: true,
+    uid: payload.uid,
+    name: payload.name,
+    email: payload.email,
+    image: payload.image,
+  }))
+  .case(Actions.signOutAction, () => ({
+    ...initialState.users,
+  }))
+  .case(Actions.editUserInfoAction, (state, payload) => ({
+    ...state,
+    name: payload.name,
+    email: payload.email,
+  }))
+  .case(Actions.editUserImageAction, (state, payload) => ({
+    ...state,
+    image: payload.image,
+  }))
+  .case(Actions.deleteUserImageAction, (state) => ({
+    ...state,
+    image: null,
+  }))
