3230876371da66bb705893f23a6ada8c43cdc50f
yuki sawabe <sawabe0v0@gmail.com>
2021-12-09 14:13:49 +0900

    rewright actions & reducers about quiz with typescript-fsa

--- front/src/reducks/quizzes/actions.ts
+++ front/src/reducks/quizzes/actions.ts
@@ -1,15 +1,6 @@
-export const FETCH_QUIZZES = "FETCH_QUIZZES";
-export const fetchQuizzesAction = (payload) => {
-  return {
-    type: "FETCH_QUIZZES",
-    payload,
-  };
-};
+import actionCreatorFactory from "typescript-fsa";
 
-export const DELETE_QUIZ = "DELETE_QUIZ";
-export const deleteQuizAction = (payload) => {
-  return {
-    type: "DELETE_QUIZ",
-    payload,
-  };
-};
+const actionCreator = actionCreatorFactory();
+
+export const fetchQuizzesAction = actionCreator<object[]>("FETCH_QUIZZES");
+export const deleteQuizAction = actionCreator<object[]>("DELETE_QUIZ");
