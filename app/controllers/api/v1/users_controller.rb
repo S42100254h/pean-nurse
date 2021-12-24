5b9d5fdb6c2e90669206c857c41967839f509808
yuki sawabe <sawabe0v0@gmail.com>
2021-12-24 12:43:32 +0900

    create endpoint to get list of users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,10 @@
 class Api::V1::UsersController < Api::V1::ApiController
   before_action :authenticate_user!
+  
+  def index
+    users = User.all
+    render json: users  
+  end
 
   def currentuser
     @user = current_user
