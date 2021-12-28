70d5b31923125d8e88972d7b06b6a83b68b4bf6a
yuki sawabe <sawabe0v0@gmail.com>
2021-12-29 06:29:57 +0900

    rubocop -a

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -6,7 +6,7 @@ class Api::V1::UsersController < Api::V1::ApiController
     users = User.all
     render json: users
   end
-  
+
   def show
     user = User.find(params[:id])
     render json: user
