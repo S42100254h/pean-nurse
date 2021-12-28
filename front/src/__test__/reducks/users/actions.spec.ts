367621f26c8928a3c37f93542cf40660b370ef26
yuki sawabe <sawabe0v0@gmail.com>
2021-12-28 18:12:58 +0900

    create test for fetchUsersAction

---
+++ front/src/__test__/reducks/users/actions.spec.ts
@@ -0,0 +1,25 @@
+import {
+  fetchUsersAction,
+  deleteUserAction,
+} from "../../../reducks/users/actions";
+
+describe ("actions.tsのテスト", () => {
+  it ("fetchUsersActionのテスト", () => {
+    const dummy = [
+      {
+        id: 1, isSignedIn: true, uid: "cat@gmail.com", name: "cat", email: "cat@gmail.com", image: null
+      },
+      {
+        id: 2, isSignedIn: true, uid: "dog@gmail.com", name: "dog", email: "dog@gmail.com", image: null
+      },
+    ];
+    const action = fetchUsersAction(dummy);
+
+    expect(action).toStrictEqual(
+      {
+        type: "FETCH_USERS",
+        payload: dummy,
+      }
+    )
+  });
+});
