c44da8d505bd93380ee9f641408e87ec311dbdd1
yuki sawabe <sawabe0v0@gmail.com>
2022-01-07 17:10:11 +0900

    update createQuiz method in operation file

--- front/src/function/quiz.ts
+++ front/src/function/quiz.ts
@@ -11,53 +11,30 @@ export const createQuiz = (quiz: string, choice1: string, select1: string, choic
       const client = localStorage.getItem("client") || "";
       const uid = localStorage.getItem("uid") || "";
       const headers = { "access-token": auth_token, client: client, uid: uid };
-      const quizApiEndpoint = process.env.REACT_APP_API_URL + "quizzes";
-      const quizBody = { title: quiz };
+      const apiEndpoint = process.env.REACT_APP_API_URL + "quizzes";
+
+      const choicesList: object[] = [];
+      if (choice1) choicesList.push({ choice: choice1, is_right: select1 });
+      if (choice2) choicesList.push({ choice: choice2, is_right: select2 });
+      if (choice3) choicesList.push({ choice: choice3, is_right: select3 });
+      if (choice4) choicesList.push({ choice: choice4, is_right: select4 });
+
+      const body = { title: quiz, choices: choicesList };
 
       axios
-        .post(quizApiEndpoint, quizBody, { headers: headers })
-        .then((resp) => {
-          const newQuizId = resp.data.id;
-          const choiceApiEndpoint = process.env.REACT_APP_API_URL + "choices";
+        .post(apiEndpoint, body, { headers: headers })
+        .then(() => {
+          dispatch(showLoadingAction("Create quiz..."));
+          dispatch(push("/quiz/create"));
 
-          const choiceBodyList: object[] =  [];
-          if (choice1) choiceBodyList.push({ choice: { choice: choice1, is_right: select1, quiz_id: newQuizId } });
-          if (choice2) choiceBodyList.push({ choice: { choice: choice2, is_right: select2, quiz_id: newQuizId } });
-          if (choice3) choiceBodyList.push({ choice: { choice: choice3, is_right: select3, quiz_id: newQuizId } });
-          if (choice4) choiceBodyList.push({ choice: { choice: choice4, is_right: select4, quiz_id: newQuizId } });
-
-          for ( let i = 0; i < choiceBodyList.length; i++) {
-            axios
-              .post(choiceApiEndpoint, choiceBodyList[i], { headers: headers })
-              .then(() => {
-                dispatch(showLoadingAction("Create quiz..."));
-                dispatch(push("/quiz/create"));
-
-                setTimeout(() => {
-                  dispatch(hideLoadingAction());
-                  dispatch(setNotificationAction({ variant: "success", message: "選択肢の作成に成功しました。" }));
-                }, 1000);
-              })
-              .catch(() => {
-                const deleteQuizApiEndpoint = process.env.REACT_APP_API_URL + "quizzes/" + newQuizId;
-                axios
-                  .delete(deleteQuizApiEndpoint, { headers: headers })
-                  .then(() => {
-                    setTimeout(() => {
-                      dispatch(setNotificationAction({ variant: "error", message: "選択肢の作成に失敗しました。ロールバックします。" }));
-                    }, 400);
-                  })
-                  .catch(() => {
-                    setTimeout(() => {
-                      dispatch(setNotificationAction({ variant: "error", message: "誤ったクイズが作成された可能性があります。管理者へ連絡してください。" }));
-                    }, 400);
-                  });
-              });
-          }
+          setTimeout(() => {
+            dispatch(hideLoadingAction());
+            dispatch(setNotificationAction({ variant: "success", message: "クイズの作成に成功しました。" }));
+          }, 1000);
         })
         .catch(() => {
           setTimeout(() => {
-            dispatch(setNotificationAction({ variant: "error", message: "問題の作成に失敗しました。" }));
+            dispatch(setNotificationAction({ variant: "error", message: "クイズの作成に失敗しました。" }));
           }, 400);
         });
     }
