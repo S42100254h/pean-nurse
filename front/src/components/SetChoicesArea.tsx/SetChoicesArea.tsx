1ed085ef7e33f14658d4444efe8e0c178aa6016c
yuki sawabe <sawabe0v0@gmail.com>
2022-01-12 17:40:13 +0900

    create basis of SetChoicesArea component

---
+++ front/src/components/SetChoicesArea.tsx/SetChoicesArea.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SetChoicesArea = () => {
+  return <></>;
+};
+
+export default SetChoicesArea;
---
+++ front/src/components/SetChoicesArea.tsx/index.ts
@@ -0,0 +1,1 @@
+export { default as SetChoicesArea } from "./SetChoicesArea";
