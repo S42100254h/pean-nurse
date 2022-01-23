75c5dfc91759a47626c659a7ccca52cc6356bd4a
yuki sawabe <sawabe0v0@gmail.com>
2022-01-23 10:03:56 +0900

    change component name

--- front/src/components/ConfirmDialog/index.ts
+++ front/src/components/ConfirmDialog/index.ts
@@ -1,4 +1,4 @@
-export { default as Choice } from "./Choice";
+export { default as ChoiceCard } from "./ChoiceCard";
 export { default as ConfirmCreateDialog } from "./ConfirmCreateDialog";
 export { default as ConfirmUpdateDialog } from "./ConfirmUpdateDialog";
 export { default as Quiz } from "./Quiz";
