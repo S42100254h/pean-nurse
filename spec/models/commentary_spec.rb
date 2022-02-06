1db2ba47eabee65724ffc43e1b541e3a032487a0
yuki sawabe <sawabe0v0@gmail.com>
2022-02-07 05:15:20 +0900

    create normal scenario test for commentary model

--- spec/models/commentary_spec.rb
+++ spec/models/commentary_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Commentary, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "normal scenario" do
+    context "text was enterd" do
+      let(:commentary) { build(:commentary) }
+      it "commentary is created" do
+        expect(commentary.valid?).to eq true
+      end
+    end
+  end
 end
