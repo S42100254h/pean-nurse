52ad69caae5c709f32a275e2193d55a1adb97c66
yuki sawabe <sawabe0v0@gmail.com>
2022-02-07 05:28:41 +0900

    create exception scenario test for commentary

--- spec/models/commentary_spec.rb
+++ spec/models/commentary_spec.rb
@@ -9,4 +9,14 @@ RSpec.describe Commentary, type: :model do
       end
     end
   end
+
+  describe "exception scenario" do
+    context "text was not entered" do
+      let(:commentary) { build(:commentary, text: nil) }
+      it "error occurs" do
+        commentary.valid?
+        expect(commentary.errors.messages[:text]).to include "can't be blank"
+      end
+    end
+  end
 end
