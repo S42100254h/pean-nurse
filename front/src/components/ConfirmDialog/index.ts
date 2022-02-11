002226be862640b6c65058159272753e5b292902
yuki sawabe <sawabe0v0@gmail.com>
2022-02-11 09:51:53 +0900

    create Commentary component

--- front/src/components/ConfirmDialog/index.ts
+++ front/src/components/ConfirmDialog/index.ts
@@ -1,4 +1,5 @@
 export { default as ChoiceCard } from "./ChoiceCard";
+export { default as Commentary } from "./Commentary";
 export { default as ConfirmCreateDialog } from "./ConfirmCreateDialog";
 export { default as ConfirmUpdateDialog } from "./ConfirmUpdateDialog";
 export { default as Quiz } from "./Quiz";
