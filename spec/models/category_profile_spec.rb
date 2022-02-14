22cd3db1e7eef90c97edc5374a11a0e3a011ebc7
yuki sawabe <sawabe0v0@gmail.com>
2022-02-15 06:32:49 +0900

    create normal scenario test for category_profile model

--- spec/models/category_profile_spec.rb
+++ spec/models/category_profile_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe CategoryProfile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "normal scenario" do
+    context "necessary information were enterd" do
+      let(:category_profile) { build(:category_profile) }
+
+      it "category_profile is created" do
+        expect(category_profile.valid?).to eq true
+      end
+    end
+  end
 end
