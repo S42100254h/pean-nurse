480dbe36c0baed994317dba730a14b858f334f15
yuki sawabe <sawabe0v0@gmail.com>
2022-02-15 12:32:48 +0900

    update category_profiles_controller

--- app/controllers/api/v1/category_profiles_controller.rb
+++ app/controllers/api/v1/category_profiles_controller.rb
@@ -1,16 +1,40 @@
 class Api::V1::CategoryProfilesController < Api::V1::ApiController
+  before_action :set_category_profile, only: [:update, :destroy]
+  before_action :authenticate_admin!, only: [:create, :update, :destroy]
+
   def index
+    category_profiles = CategoryProfile.all
+    render json: category_profiles
   end
 
   def show
+    category_profile = CategoryProfile.find(params[:id])
+    render json: category_profile
   end
 
   def create
+    category = Category.find(params[:category_profile][:category_id])
+    category_profile = category.create_category_profile!(category_profile_params)
+    render json: category_profile
   end
 
   def update
+    @category_profile.update!(category_profile_params)
+    render json: @category_profile
   end
 
   def destroy
+    @category_profile.destroy!
+    render json: @category_profile
+  end
+
+  private
+
+  def set_category_profile
+    @category_profile = CategoryProfile.find(params[:id])
+  end
+
+  def category_profile_params
+    params.require(:category_profile).permit(:title, :image, :caption, :uid, :category_id)
   end
 end
