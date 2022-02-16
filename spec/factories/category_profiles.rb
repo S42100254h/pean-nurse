60ca4d0d4b94e1dafe0c36162a26905680f2aa0e
yuki sawabe <sawabe0v0@gmail.com>
2022-02-16 20:03:22 +0900

    fix factories about category_profile

--- spec/factories/category_profiles.rb
+++ spec/factories/category_profiles.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :category_profile do
     title { Faker::Lorem.word }
-    image { Faker::Avatar.image }
+    image { Rack::Test::UploadedFile.new Rails.root.join "front/src/assets/img/cat.png" }
     caption { Faker::Lorem.sentence }
     uid { Faker::Lorem.word }
     category
