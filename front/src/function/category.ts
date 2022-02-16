af89a0ba6e0725cc1e0ff6d9f2d16a1b03279822
yuki sawabe <sawabe0v0@gmail.com>
2022-02-16 19:33:23 +0900

    enable to upload image

--- front/src/function/category.ts
+++ front/src/function/category.ts
@@ -15,15 +15,24 @@ export const createCategory = (category: string, caption?: string, image?: File 
       const auth_token = localStorage.getItem("access-token") || "";
       const client = localStorage.getItem("client") || "";
       const userId = localStorage.getItem("uid") || "";
-      const headers = { "access-token": auth_token, client: client, uid: userId };
       const apiEndpoint = process.env.REACT_APP_API_URL + "categories";
-      const body: Body = { category: { name: category } };
-      if (caption && image && uid) {
-        body.category_profile = { title: category, caption: caption, image: image, uid: uid };
-      }
+
+      let form: any = new FormData();
+      form.append("name", category);
+      form.append("title", category);
+      form.append("caption", caption);
+      form.append("image", image);
+      form.append("uid", uid);
 
       axios
-        .post(apiEndpoint, body, { headers: headers })
+        .post(apiEndpoint, form, {
+          headers: {
+            "content-type": "multipart/form-data",
+            "access-token": auth_token,
+            client: client,
+            uid: userId,
+          },
+        })
         .then(() => {
           dispatch(showLoadingAction("Create category..."));
           dispatch(push("/category/create"));
