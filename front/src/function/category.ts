3e17cad20f2422d7272821e300b3cba9a074d86d
yuki sawabe <sawabe0v0@gmail.com>
2022-02-16 17:37:36 +0900

    refactor createCategory method

--- front/src/function/category.ts
+++ front/src/function/category.ts
@@ -4,6 +4,11 @@ import { setNotificationAction } from "../reducks/notification/actions";
 import { push } from "connected-react-router";
 import { Dispatch } from "redux";
 
+type Body = {
+  category: Object;
+  category_profile?: Object;
+};
+
 export const createCategory = (category: string, caption?: string, image?: File | null, uid?: string) => {
   return async (dispatch: Dispatch) => {
     if (localStorage.getItem("access-token")) {
@@ -12,13 +17,10 @@ export const createCategory = (category: string, caption?: string, image?: File 
       const userId = localStorage.getItem("uid") || "";
       const headers = { "access-token": auth_token, client: client, uid: userId };
       const apiEndpoint = process.env.REACT_APP_API_URL + "categories";
-      const body =
-        caption && image && uid
-          ? {
-            category: { name: category },
-            category_profile: { title: category, caption: caption, image: image, uid: uid },
-          }
-          : { category: { name: category } };
+      const body: Body = { category: { name: category } };
+      if (caption && image && uid) {
+        body.category_profile = { title: category, caption: caption, image: image, uid: uid };
+      }
 
       axios
         .post(apiEndpoint, body, { headers: headers })
