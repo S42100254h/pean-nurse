7ca9bf2706451f4e73d834fe772e5e5b42abab14
yuki sawabe <sawabe0v0@gmail.com>
2022-02-17 17:03:43 +0900

    enable factory about category to use trait for creating category_profile

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,5 +1,12 @@
 FactoryBot.define do
   factory :category do
     sequence(:name) {|n| "#{n}_#{Faker::Lorem.word}" }
+
+    trait :with_category_profile do
+      title { Faker::Lorem.word }
+      image { Rack::Test::UploadedFile.new Rails.root.join "front/src/assets/img/cat.png" }
+      caption { Faker::Lorem.sentence }
+      uid { Faker::Lorem.word }
+    end
   end
 end
