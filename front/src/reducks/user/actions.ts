a9db46dcc9bfdbee7382eb0d27e8d4b8e5e95fe0
yuki sawabe <sawabe0v0@gmail.com>
2022-02-27 19:15:27 +0900

    create action to update experience point

--- front/src/reducks/user/actions.ts
+++ front/src/reducks/user/actions.ts
@@ -6,6 +6,7 @@ const actionCreator = actionCreatorFactory();
 export const signUpAction = actionCreator<User>("SIGN_UP");
 export const signInAction = actionCreator<User>("SIGN_IN");
 export const signOutAction = actionCreator("SIGN_OUT");
+export const editExperiencePointAction = actionCreator<Partial<User>>("EDIT_EXPERIENCE_POINT");
 export const editUserInfoAction = actionCreator<User>("EDIT_USER_INFO");
 export const editUserImageAction = actionCreator<User>("EDIT_USER_IMAGE");
 export const deleteUserImageAction = actionCreator("DELETE_USER_IMAGE");
