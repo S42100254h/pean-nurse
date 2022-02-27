756c8c4a24199f410c3f8a48736ed35cc6697a9b
yuki sawabe <sawabe0v0@gmail.com>
2022-02-27 14:36:06 +0900

    fix inquiries_spec

--- spec/requests/api/v1/inquiries_spec.rb
+++ spec/requests/api/v1/inquiries_spec.rb
@@ -2,9 +2,11 @@ require "rails_helper"
 
 RSpec.describe "Api::V1::Inquiries", type: :request do
   describe "POST /api/v1/inquiries/create" do
-    subject { post(api_v1_inquiries_create_path, params: params) }
+    subject { post(api_v1_inquiries_create_path, params: params, headers: headers) }
 
     context "send correct inquiry information" do
+      let(:headers) { user.create_new_auth_token }
+      let!(:user) { create(:user) }
       let(:params) { attributes_for(:inquiry) }
 
       it "User can send email" do
