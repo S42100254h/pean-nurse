8f2896a7a6d2d8cd00c275099e27d2ca6d95a17d
yuki sawabe <sawabe0v0@gmail.com>
2022-03-03 21:06:57 +0900

    update experiences_controller

--- app/controllers/api/v1/experiences_controller.rb
+++ app/controllers/api/v1/experiences_controller.rb
@@ -1,16 +1,39 @@
 class Api::V1::ExperiencesController < Api::V1::ApiController
+  before_action :set_experience, only: [:update, :destroy]
+  before_action :authenticate_admin!, only: [:create, :update, :destroy]
+
   def index
+    experiences = Experience.all
+    render json: experiences
   end
 
   def show
+    experience = Experience.find(params[:id])
+    render json: experience
   end
 
   def create
+    experience = Experience.create!(experience_params)
+    render json: experience
   end
 
   def update
+    @experience.update!(experience_params)
+    render json: @experience
   end
 
   def destroy
+    @experience.destroy!
+    render json: @experience
   end
+
+  private
+
+    def set_experience
+      @experience = Experience.find(params[:id])
+    end
+
+    def experience_params
+      params.require(:experience).permit(:level, :experience)
+    end
 end
