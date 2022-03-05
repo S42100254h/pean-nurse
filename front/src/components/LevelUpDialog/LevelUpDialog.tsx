9ed0113e360e786a6b186a5035dbc294b0da2c0c
yuki sawabe <sawabe0v0@gmail.com>
2022-03-05 13:44:56 +0900

    enable to display animation of getting experience

--- front/src/components/LevelUpDialog/LevelUpDialog.tsx
+++ front/src/components/LevelUpDialog/LevelUpDialog.tsx
@@ -1,11 +1,13 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Dialog, DialogContent } from "@material-ui/core";
 import { editExperiencePoint } from "../../reducks/user/operations";
 import { RootState } from "../../types/entity/rootState";
-import { getExperiencePoint } from "../../reducks/user/selectors";
+import { getExperiencePoint, getUserLevel } from "../../reducks/user/selectors";
+import { ProgressBar } from "../UIkit";
 import cat from "../../assets/img/cat.png";
 import styled from "styled-components";
+import axios from "axios";
 
 const Container = styled.div`
   margin: 10px auto;
@@ -35,14 +37,41 @@ type Props = {
 const LevelUpDialog = (props: Props) => {
   const dispatch = useDispatch();
   const selector = useSelector((state: RootState) => state);
-
   const exp = getExperiencePoint(selector);
+  const level = getUserLevel(selector);
+
+  const [endExp, setEndExp] = useState(0),
+    [startExp, setStartExp] = useState(0);
+
+  // add "| 0" to prevent from returning infinity (0 / number => inifinity)
+  const percent = (((exp - startExp) / (endExp - startExp)) * 100) | 0;
+
+  useEffect(() => {
+    const startApiEndpoint = process.env.REACT_APP_API_URL + "experiences/" + level;
+    // get experience of next level
+    const endApiEndpoint = process.env.REACT_APP_API_URL + "experiences/" + (Number(level) + 1);
+    let isMounted = true;
+
+    axios.get(startApiEndpoint).then((resp) => {
+      if (isMounted) {
+        setStartExp(resp.data.experience);
+      }
+    });
+    axios.get(endApiEndpoint).then((resp) => {
+      if (isMounted) {
+        setEndExp(resp.data.experience);
+      }
+    });
+
+    return () => {
+      isMounted = false;
+    };
+  }, []);
 
   useEffect(() => {
     if (props.open === false) return;
-
     setTimeout(() => {
-      dispatch(editExperiencePoint(100));
+      dispatch(editExperiencePoint(30));
     }, 200);
   }, [props.open]);
 
@@ -53,6 +82,7 @@ const LevelUpDialog = (props: Props) => {
           <Container>
             <Heading>経験値アップ！！</Heading>
             <Image src={cat} alt="ねこ" width="180px" height="180px" />
+            <ProgressBar percent={percent} />
           </Container>
         </DialogContent>
       </Dialog>
