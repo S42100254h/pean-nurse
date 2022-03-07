a209abc49a1992c94218e782f08d34d198a101eb
yuki sawabe <sawabe0v0@gmail.com>
2022-03-07 17:43:51 +0900

    create relations about badge

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,7 @@
 class Category < ApplicationRecord
   has_many :category_quiz_relations, dependent: :destroy
   has_many :quizzes, through: :category_quiz_relations
+  has_many :badges, dependent: :destroy
   has_one :category_profile, dependent: :destroy
   validates :name, presence: true
   validates :name, uniqueness: true
