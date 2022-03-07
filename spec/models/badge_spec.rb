b58df7b97c556ef5113ace6380d0097168db3eb4
yuki sawabe <sawabe0v0@gmail.com>
2022-03-07 18:28:08 +0900

    create normal scenario test for badge model

--- spec/models/badge_spec.rb
+++ spec/models/badge_spec.rb
@@ -1,5 +1,15 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Badge, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "normal scenario" do
+    context "correct data is entered" do
+      let(:category) { create(:category) }
+      let(:user) { create(:user) }
+      let(:badge) { build(:badge, user_id: user.id, category_id: category.id) }
+
+      it "badge is created" do
+        expect(badge.valid?).to eq true
+      end
+    end
+  end
 end
