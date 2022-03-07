300903f5b50ac91df69621a2600dab7596449640
yuki sawabe <sawabe0v0@gmail.com>
2022-03-07 17:49:00 +0900

    add validation to badge model

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,4 +1,7 @@
 class Badge < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  validates :color, inclusion: { in: %w(gold silver bronze) }
+  validates :color, presence: true
+  validates :index, presence: true
 end
