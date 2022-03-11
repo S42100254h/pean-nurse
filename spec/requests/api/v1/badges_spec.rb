8b45507620b3b31cc474c26ebfe57611e270d807
yuki sawabe <sawabe0v0@gmail.com>
2022-03-11 15:26:02 +0900

    update test about badge

--- spec/requests/api/v1/badges_spec.rb
+++ spec/requests/api/v1/badges_spec.rb
@@ -5,13 +5,27 @@ RSpec.describe "Api::V1::Badges", type: :request do
     subject { post(api_v1_badges_path, params: params, headers: headers) }
 
     context "send correct badge information" do
-      let(:params) { { badge: attributes_for(:badge), user_id: current_user.id, category_id: category.id } }
+      let(:params) { { badge: attributes_for(:badge), category_id: category.id } }
       let(:headers) { current_user.create_new_auth_token }
       let!(:category) { create(:category) }
       let!(:current_user) { create(:user) }
 
       it "Badge is created" do
         expect { subject }.to change { Badge.count }.by(1)
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context "badge has already existed" do
+      let(:params) { { badge: attributes_for(:badge, index: 1), category_id: category.id } }
+      let(:headers) { current_user.create_new_auth_token }
+      let!(:badge) { create(:badge, index: 1, color: "bronze", category_id: category.id) }
+      let!(:category) { create(:category) }
+      let!(:current_user) { create(:user) }
+
+      it "Badge color is updated" do
+        expect { subject }.to change { Badge.count }.by(0) &
+                              change { badge.reload.color }.from("bronze").to("silver")
         expect(response).to have_http_status(200)
       end
     end
@@ -54,10 +68,10 @@ RSpec.describe "Api::V1::Badges", type: :request do
   describe "PATCH /api/v1/badges/:id" do
     subject { patch(api_v1_badge_path(badge.id), params: params, headers: headers) }
 
-    let(:params) { { badge: attributes_for(:badge), index: 1, color: "gold", user_id: current_user.id, category_id: category.id } }
+    let(:params) { { badge: attributes_for(:badge, index: 1, color: "gold"), user_id: current_user.id, category_id: category.id } }
     let(:headers) { current_user.create_new_auth_token }
     let(:current_user) { create(:user) }
-    let(:badge) { create(:badge, index: 1, color: "silver", user_id: current_user.id, category_id: category.id) }
+    let!(:badge) { create(:badge, index: 1, color: "silver", user_id: current_user.id, category_id: category.id) }
     let(:category) { create(:category) }
 
     it "badge is updated" do
