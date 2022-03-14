1cf1ba4854be4658dae9464e6b5278728f24457b
yuki sawabe <sawabe0v0@gmail.com>
2022-03-14 16:28:47 +0900

    modify selectors about badges

--- front/src/reducks/badges/selectors.ts
+++ front/src/reducks/badges/selectors.ts
@@ -3,4 +3,8 @@ import { RootState } from "../../types/entity/rootState";
 
 const badgesSelector = (state: RootState) => state.badges;
 
-export const getBadges = createSelector([badgesSelector], (state) => state.list);
+export const getBronzeBadges = createSelector([badgesSelector], (state) => state.bronze);
+
+export const getSilverBadges = createSelector([badgesSelector], (state) => state.silver);
+
+export const getGoldBadges = createSelector([badgesSelector], (state) => state.gold);
