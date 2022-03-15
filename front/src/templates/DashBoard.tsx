1984ad81e01d54f6ea055d146ac5c8e80aeba848
yuki sawabe <sawabe0v0@gmail.com>
2022-03-15 11:34:57 +0900

    add ListArea to DashBoard

--- front/src/templates/DashBoard.tsx
+++ front/src/templates/DashBoard.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Profile } from "../components/Profile";
+import { ListArea } from "../components/ListArea";
 import styled from "styled-components";
 import { Spacer } from "../components/UIkit";
 import bronze from "../assets/img/bronze.png";
@@ -10,8 +11,6 @@ import { RootState } from "../types/entity/rootState";
 import { fetchBadges } from "../reducks/badges/operations";
 import { fetchCategories } from "../reducks/categories/operations";
 import { getBronzeBadges, getSilverBadges, getGoldBadges } from "../reducks/badges/selectors";
-import { getCategories } from "../reducks/categories/selectors";
-import cat from "../assets/img/cat.png";
 
 const Container = styled.div`
   width: calc(100% - 5rem);
@@ -21,43 +20,8 @@ const Container = styled.div`
   margin: 0 auto;
 `;
 
-const Text = styled.p`
-  font-size: 14px;
-`;
-
 const Image = styled.img`
   margin: 5px 0 10px 0;
-`;
-
-const Icon = styled.img`
-  width: 45px;
-  height: 45px;
-  display: inline-block;
-  margin-right: 5px;
-`;
-
-const Caption = styled.p`
-  font-size: 16px;
-  line-height: 60px;
-  display: inline-block;
-`;
-
-const BadgeContainer = styled.div`
-  width: 180px;
-  height: 60px;
-  border: 1px solid rgba(0, 0, 0, 0.1);
-  border-radius: 4px;
-  background-color: #fff;
-  display: inline-block;
-  margin: 5px;
-  text-align: center;
-  transition-duration: 0.3s;
-  &:hover {
-    transition-duration: 0.5s;
-    box-shadow: 0 0 8px grey;
-    opacity: 0.75;
-    cursor: pointer;
-  }
 `;
 
 const DashBoard = () => {
@@ -66,12 +30,6 @@ const DashBoard = () => {
   const bronzeBadges = getBronzeBadges(selector);
   const silverBadges = getSilverBadges(selector);
   const goldBadges = getGoldBadges(selector);
-  const categories = getCategories(selector);
-
-  const findCategoryName = (category_id: number) => {
-    const selectedCategory = categories.filter((category) => category.id === category_id);
-    return selectedCategory[0].name;
-  };
 
   useEffect(() => {
     dispatch(fetchBadges());
@@ -84,48 +42,15 @@ const DashBoard = () => {
       <Spacer size="sm" />
       <Container>
         <Image src={bronze} />
-        <div>
-          {bronzeBadges.length === 0 && <Text>学習中の問題はありません。</Text>}
-          {bronzeBadges.map((bronzeBadge) => (
-            <BadgeContainer key={bronzeBadge.id}>
-              <Icon src={cat} />
-              <Caption>
-                {findCategoryName(bronzeBadge.category_id)}
-                {bronzeBadge.index}
-              </Caption>
-            </BadgeContainer>
-          ))}
-        </div>
+        <ListArea badges={bronzeBadges} />
       </Container>
       <Container>
         <Image src={silver} />
-        <div>
-          {silverBadges.length === 0 && <Text>学習中の問題はありません。</Text>}
-          {silverBadges.map((silverBadge) => (
-            <BadgeContainer key={silverBadge.id}>
-              <Icon src={cat} />
-              <Caption>
-                {findCategoryName(silverBadge.category_id)}
-                {silverBadge.index}
-              </Caption>
-            </BadgeContainer>
-          ))}
-        </div>
+        <ListArea badges={silverBadges} />
       </Container>
       <Container>
         <Image src={gold} />
-        <div>
-          {goldBadges.length === 0 && <Text>学習中の問題はありません。</Text>}
-          {goldBadges.map((goldBadge) => (
-            <BadgeContainer key={goldBadge.id}>
-              <Icon src={cat} />
-              <Caption>
-                {findCategoryName(goldBadge.category_id)}
-                {goldBadge.index}
-              </Caption>
-            </BadgeContainer>
-          ))}
-        </div>
+        <ListArea badges={goldBadges} />
       </Container>
     </div>
   );
