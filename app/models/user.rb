eb9e42d53254a5606752a7742c86520481545792
yuki sawabe <sawabe0v0@gmail.com>
2022-03-15 20:16:59 +0900

    add relation between user and inquiry

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@
 
 class User < ApplicationRecord
   has_many :badges, dependent: :destroy
+  has_many :inquiries, dependent: :destroy
   extend Devise::Models
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
