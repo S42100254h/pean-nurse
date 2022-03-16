a453cd9829e3a647dccbd546a1538c64c4e9a84a
yuki sawabe <sawabe0v0@gmail.com>
2022-03-16 09:25:13 +0900

    create test about stack model

--- spec/models/stack_spec.rb
+++ spec/models/stack_spec.rb
@@ -1,5 +1,22 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe Stack, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "normal scenario" do
+    context "user exists" do
+      let(:stack) { build(:stack) }
+      it "stack is created" do
+        expect(stack.valid?).to eq true
+      end
+    end
+  end
+
+  describe "exception scenario" do
+    context "user doesn't exist" do
+      let(:stack) { build(:stack, user_id: nil) }
+      it "error occurs" do
+        stack.valid?
+        expect(stack.errors.messages[:user]).to include "must exist"
+      end
+    end
+  end
 end
