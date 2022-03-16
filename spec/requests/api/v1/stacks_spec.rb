e59bb06b2ddb886a9f96b596bdd08d2b89c15f61
yuki sawabe <sawabe0v0@gmail.com>
2022-03-16 09:58:53 +0900

    create test about GET /api/v1/stacks

--- spec/requests/api/v1/stacks_spec.rb
+++ spec/requests/api/v1/stacks_spec.rb
@@ -12,4 +12,25 @@ RSpec.describe "Api::V1::Stacks", type: :request do
       expect(response).to have_http_status(200)
     end
   end
+
+  describe "GET /api/v1/stacks" do
+    subject { get(api_v1_stacks_path, headers: headers) }
+
+    before do
+      create(:user, id: 1)
+      create(:user, id: 2)
+      create_list(:stack, 4, user_id: 1)
+      create_list(:stack, 8, user_id: 2)
+    end
+
+    let(:headers) { current_user.create_new_auth_token }
+    let(:current_user) { User.find(1) }
+
+    it "get stacks" do
+      subject
+      res = JSON.parse(response.body)
+      expect(res.length).to eq 4
+      expect(response).to have_http_status(200)
+    end
+  end
 end
