62d4ae8a5cf07b7528ac15f568f21b00b8e83784
yuki sawabe <sawabe0v0@gmail.com>
2022-03-16 11:24:00 +0900

    create reducers about stack & test file

---
+++ front/src/__test__/reducks/stacks/reducers.spec.ts
@@ -0,0 +1,46 @@
+import initialState from "../../../reducks/store/initialState";
+import { StacksReducer } from "../../../reducks/stacks/reducers";
+import { fetchStacksAction } from "../../../reducks/stacks/actions";
+import { Stacks } from "../../../types/entity/stacks";
+
+describe("StacksReducerのテスト", () => {
+  const init = initialState.stacks;
+
+  it("action.type === FETCH_STACKS", () => {
+    const dummy: Stacks = {
+      list: [
+        {
+          id: 1,
+          user_id: 1,
+          created_at: "2021-01-01",
+          updated_at: "2021-02-01",
+        },
+
+        {
+          id: 2,
+          user_id: 1,
+          created_at: "2021-01-01",
+          updated_at: "2021-02-01",
+        },
+      ],
+    };
+    const action = fetchStacksAction(dummy);
+    const newState = StacksReducer(init, action);
+
+    expect(newState.list).toStrictEqual([
+      {
+        id: 1,
+        user_id: 1,
+        created_at: "2021-01-01",
+        updated_at: "2021-02-01",
+      },
+
+      {
+        id: 2,
+        user_id: 1,
+        created_at: "2021-01-01",
+        updated_at: "2021-02-01",
+      },
+    ]);
+  });
+});
