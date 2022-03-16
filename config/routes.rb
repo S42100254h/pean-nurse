45bba5024cd4648cbbcf6482a03c558f02f6381a
yuki sawabe <sawabe0v0@gmail.com>
2022-03-16 09:38:30 +0900

    modify routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,20 +5,21 @@ Rails.application.routes.draw do
         registrations: "api/v1/auth/registrations",
       }
       mount_devise_token_auth_for "Admin", at: "admin"
-      get "users/currentuser"
       get "admins/currentadmin"
+      post "inquiries/create"
+      get "users/currentuser"
       patch "users/add_exp"
       patch "users/level_up"
-      resources :users, only: [:show, :index, :update, :destroy]
-      resources :quizzes
+      get "quizzes/exam/:category_profile_uid/:exam_id", to: "quizzes#exam_index", as: "exam_quizzes"
+      resources :badges
       resources :categories
       resources :category_profiles
       resources :choices
       resources :commentaries
       resources :experiences
-      resources :badges
-      post "inquiries/create"
-      get "quizzes/exam/:category_profile_uid/:exam_id", to: "quizzes#exam_index", as: "exam_quizzes"
+      resources :quizzes
+      resources :stacks, only: [:index, :create]
+      resources :users, only: [:show, :index, :update, :destroy]
     end
   end
 
--- spec/requests/api/v1/stacks_spec.rb
+++ spec/requests/api/v1/stacks_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Stacks", type: :request do
   describe "POST /api/v1/stacks" do
-
+    
   end
 end
