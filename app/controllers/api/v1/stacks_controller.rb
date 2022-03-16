82389b0282e4e99c06ae704c126b4fff66b2f348
yuki sawabe <sawabe0v0@gmail.com>
2022-03-16 09:36:33 +0900

    update stacks_controller

--- app/controllers/api/v1/stacks_controller.rb
+++ app/controllers/api/v1/stacks_controller.rb
@@ -1,2 +1,13 @@
-class Api::V1::StacksController < ApplicationController
+class Api::V1::StacksController < Api::V1::ApiController
+  before_action :authenticate_user!
+
+  def index
+    stacks = current_user.stacks
+    render json: stacks
+  end
+
+  def create
+    stack = current_user.stacks.create!
+    render json: stack
+  end
 end
--- spec/requests/api/v1/stacks_spec.rb
+++ spec/requests/api/v1/stacks_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "Api::V1::Stacks", type: :request do
+  describe "POST /api/v1/stacks" do
 
+  end
 end
