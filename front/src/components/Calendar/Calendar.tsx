b1433efff7888fc471251eb39e0002bec6f08628
yuki sawabe <sawabe0v0@gmail.com>
2022-03-16 22:15:45 +0900

    enable to set color

--- front/src/components/Calendar/Calendar.tsx
+++ front/src/components/Calendar/Calendar.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { getStacks } from "../../reducks/stacks/selectors";
+import { RootState } from "../../types/entity/rootState";
 import styled from "styled-components";
 import dayjs from "dayjs";
 
@@ -47,16 +50,47 @@ const createCalendar = (year: number, month: number) => {
 };
 
 const days = ["日", "月", "火", "水", "木", "金", "土"];
-const schedules = createCalendar(2021, 3);
+const today = dayjs();
+const year = today.year();
+const month = today.month() + 1;
+const schedules = createCalendar(year, month);
 
 const Calendar = () => {
+  const selector = useSelector((state: RootState) => state);
+  const stacks = getStacks(selector);
+
+  const setColor = () => {
+    let color = [];
+    // take only created_at out from stacks
+    const stacksDays = stacks.map((stack) => dayjs(stack["created_at"]).format("YYYYMMDD"));
+    for (let i = 0; i < schedules.length; i++) {
+      const count = stacksDays.filter((stacksDay) => stacksDay === schedules[i].format("YYYYMMDD")).length;
+      if (count >= 15) {
+        color[i] = "dark";
+      } else if (count >= 10) {
+        color[i] = "little dark";
+      } else if (count >= 5) {
+        color[i] = "little light";
+      } else if (count >= 1) {
+        color[i] = "light";
+      } else {
+        color[i] = "white";
+      }
+    }
+    return color;
+  };
+
+  const colors = setColor();
+
   return (
     <Container>
       {days.map((day) => (
         <Day key={day.toString()}>{day}</Day>
       ))}
-      {schedules.map((schedule) => (
-        <Item key={schedule.toString()} />
+      {schedules.map((schedule, i) => (
+        <Item key={schedule.toString()} color={colors[i]}>
+          {schedule.date()}
+        </Item>
       ))}
     </Container>
   );
