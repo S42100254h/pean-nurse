84d427d47b469e25c3a1ffc111e4820e8fe11c27
yuki sawabe <sawabe0v0@gmail.com>
2022-03-16 09:49:00 +0900

    create test about POST /api/v1/stacks

--- spec/requests/api/v1/stacks_spec.rb
+++ spec/requests/api/v1/stacks_spec.rb
@@ -2,6 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::Stacks", type: :request do
   describe "POST /api/v1/stacks" do
-    
+    subject { post(api_v1_stacks_path, headers: headers) }
+
+    let(:headers) { current_user.create_new_auth_token }
+    let(:current_user) { create(:user) }
+
+    it "Stack is created" do
+      expect { subject }.to change { Stack.count }.by(1)
+      expect(response).to have_http_status(200)
+    end
   end
 end
