aa9f185c30be17a9a89b25c25c87d1680de401d3
yuki sawabe <sawabe0v0@gmail.com>
2022-03-17 17:45:38 +0900

    enable to display tooltip on bottom

--- front/src/components/UIkit/Tooltip.tsx
+++ front/src/components/UIkit/Tooltip.tsx
@@ -7,32 +7,62 @@ const Container = styled.div`
 `;
 
 type CustomProps = {
+  location: "top" | "bottom";
   width?: number;
 };
 
 const Content = styled.div<CustomProps>`
   position: absolute;
-  bottom: calc(100% + 5px);
-  left: -10px;
   width: ${(props) => (props.width ? props.width : 300)}px;
   border-radius: 4px;
   color: #fff;
   padding: 5px 10px;
   opacity: 0.8;
   background-color: ${(props) => props.theme.palette.basic.dark};
+  z-index: 100;
+  ${(props) => getLocation(props)}
   &:before {
     content: "";
     position: absolute;
-    top: 100%;
-    left: 30px;
     border: 6px solid transparent;
-    border-top-color: ${(props) => props.theme.palette.basic.dark};
+    ${(props) => getBeforeLocation(props)}
   }
 `;
+
+const getLocation = (props: any) => {
+  if (props.location === "top") {
+    return `
+      bottom: calc(100% + 5px);
+      left: -20%;
+    `;
+  } else if (props.location === "bottom") {
+    return `
+      top: calc(100% + 5px);
+      left: -310%;
+    `;
+  }
+};
+
+const getBeforeLocation = (props: any) => {
+  if (props.location === "top") {
+    return `
+        top: 100%;
+        left: 30px;
+        border-top: 6px solid #3e3e3e;
+  `;
+  } else if (props.location === "bottom") {
+    return `
+        bottom: 100%;
+        left: 50%;
+        border-bottom: 6px solid #3e3e3e;
+    `;
+  }
+};
 
 type Props = {
   content: string;
   children: ReactNode;
+  location: "top" | "bottom";
   width?: number;
 };
 
@@ -44,7 +74,11 @@ const Tooltip = (props: Props) => {
       <div onMouseEnter={() => setShow(true)} onMouseLeave={() => setShow(false)}>
         {props.children}
       </div>
-      {show && <Content width={props.width}>{props.content}</Content>}
+      {show && (
+        <Content width={props.width} location={props.location}>
+          {props.content}
+        </Content>
+      )}
     </Container>
   );
 };
