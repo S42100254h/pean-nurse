09e6347242600a205adeb2cdcac9b844fd454518
yuki sawabe <sawabe0v0@gmail.com>
2022-03-18 11:06:14 +0900

    delete unnecessary code

--- front/src/templates/CreateCategory.tsx
+++ front/src/templates/CreateCategory.tsx
@@ -1,14 +1,9 @@
 import React, { useCallback, useState } from "react";
 import { useDispatch } from "react-redux";
-import { Add, Close } from "@material-ui/icons";
 import { PrimaryButton, Spacer, TextInput } from "../components/UIkit";
 import { SetCategoryProfile } from "../components/SetCategoryProfile";
 import { createCategory } from "../function/category";
 import styled from "styled-components";
-
-type LabelProps = {
-  open: boolean;
-};
 
 const Container = styled.div`
   margin: 30px auto;
@@ -27,94 +22,44 @@ const Heading = styled.h2`
   text-align: center;
 `;
 
-const Label = styled.label<LabelProps>`
-  color: #fff;
-  display: flex;
-  justify-content: center;
-  width: 100px;
-  padding: 8px;
-  border-radius: 20px;
-  background-color: #4dd0e1;
-  cursor: pointer;
-  font-size: 12px;
-  float: left;
-  margin-bottom: 15px;
-  &:hover {
-    opacity: 0.7;
-  }
-  background-color: ${(props) => (props.open ? props.theme.palette.secondary.main : props.theme.palette.primary.main)};
-`;
-
-const StyledAdd = styled(Add)`
-  float: right;
-  color: #f5f5f5;
-  font-size: 14px;
-  margin-right: 3px;
-`;
-
-const StyledClose = styled(Close)`
-  float: right;
-  color: #f5f5f5;
-  font-size: 14px;
-  margin-right: 3px;
-`;
-
 const CreateCategory = () => {
   const dispatch = useDispatch();
 
-  const [category, setCategory] = useState(""),
-    [open, setOpen] = useState(false),
+  const [name, setName] = useState(""),
     [caption, setCaption] = useState(""),
     [image, setImage] = useState<File | null>(null),
     [fileUrl, setFileUrl] = useState<string>(""),
     [uid, setUid] = useState("");
 
-  const inputCategory = useCallback(
+  const inputName = useCallback(
     (event) => {
-      setCategory(event.target.value);
+      setName(event.target.value);
     },
-    [setCategory],
+    [setName],
   );
-
-  const handleOpenToggle = () => {
-    setOpen(!open);
-  };
 
   return (
     <Container>
       <Heading>カテゴリー作成</Heading>
       <Spacer size="sm" />
-      <TextInput
-        fullWidth={true}
-        label={"カテゴリー名"}
-        required={true}
-        rows={1}
-        value={category}
-        onChange={inputCategory}
-      />
+      <TextInput fullWidth={true} label={"カテゴリー名"} required={true} rows={1} value={name} onChange={inputName} />
       <Spacer size="xs" />
-      <Label onClick={handleOpenToggle} open={open}>
-        {!open ? <StyledAdd /> : <StyledClose />}
-        {!open ? "詳細を追加" : "閉じる"}
-      </Label>
-      {open && (
-        <SetCategoryProfile
-          image={image}
-          fileUrl={fileUrl}
-          caption={caption}
-          uid={uid}
-          setImage={setImage}
-          setFileUrl={setFileUrl}
-          setCaption={setCaption}
-          setUid={setUid}
-        />
-      )}
+      <SetCategoryProfile
+        image={image}
+        fileUrl={fileUrl}
+        caption={caption}
+        uid={uid}
+        setImage={setImage}
+        setFileUrl={setFileUrl}
+        setCaption={setCaption}
+        setUid={setUid}
+      />
       <Spacer size="xs" />
       <PrimaryButton
         label={"カテゴリーを作成する"}
         fullWidth={true}
-        disabled={open ? !category || !caption || !image || !uid : !category}
-        onClick={() => dispatch(createCategory(category, caption, image, uid))}
+        disabled={!name || !caption || !image || !uid}
+        onClick={() => dispatch(createCategory(name, caption, image, uid))}
       />
     </Container>
   );
