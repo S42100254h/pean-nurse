67f8c807a367847d302c72160c47e1b66acfa2ac
yuki sawabe <sawabe0v0@gmail.com>
2022-03-18 17:47:22 +0900

    delete unnecessary instance method

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,17 +2,10 @@ class Category < ApplicationRecord
   has_many :category_quiz_relations, dependent: :destroy
   has_many :quizzes, through: :category_quiz_relations
   has_many :badges, dependent: :destroy
-  has_one :category_profile, dependent: :destroy
   validates :name, presence: true
   validates :name, uniqueness: true
   validates :image, presence: true
   validates :caption, presence: true
   validates :uid, presence: true
   mount_uploader :image, ImageUploader
-
-  def create_with_category_profile(title, caption, image, uid)
-    profile_items = { title: title, caption: caption, image: image, uid: uid }
-    category_profile = self.create_category_profile!(profile_items)
-    category_profile.save!
-  end
 end
