f47f9f5559c10ef025cff60982e71d88c034f22e
yuki sawabe <sawabe0v0@gmail.com>
2022-03-18 10:37:13 +0900

    modify factory about category

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -4,12 +4,5 @@ FactoryBot.define do
     image { Rack::Test::UploadedFile.new Rails.root.join "front/src/assets/img/pig.png" }
     caption { Faker::Lorem.sentence }
     uid { Faker::Lorem.word }
-
-    trait :with_category_profile do
-      title { Faker::Lorem.word }
-      image { Rack::Test::UploadedFile.new Rails.root.join "front/src/assets/img/pig.png" }
-      caption { Faker::Lorem.sentence }
-      uid { Faker::Lorem.word }
-    end
   end
 end
