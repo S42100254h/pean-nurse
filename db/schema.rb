53fbf8259c798da58bb540d553f552c26c8d3679
yuki sawabe <sawabe0v0@gmail.com>
2022-03-18 10:05:04 +0900

    rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_15_235406) do
+ActiveRecord::Schema.define(version: 2022_03_18_010020) do
 
   create_table "admins", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "provider", default: "email", null: false
@@ -52,6 +52,9 @@ ActiveRecord::Schema.define(version: 2022_03_15_235406) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image"
+    t.string "caption"
+    t.string "uid"
     t.index ["name"], name: "index_categories_on_name", unique: true
   end
 
