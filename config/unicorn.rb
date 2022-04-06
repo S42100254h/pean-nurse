27b61729af2de67e12b7a4baf2e7ca886a7240aa
yuki sawabe <sawabe0v0@gmail.com>
2022-04-06 19:22:10 +0900

    rubocop -a & delete unnecessary puts command

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -2,32 +2,28 @@ worker_processes 3
 timeout 15
 preload_app true
 
-rails_root = File.expand_path("../../", __FILE__)
+rails_root = File.expand_path("..", __dir__)
 
 working_directory rails_root
 
 # listen "#{rails_root}/tmp/unicorn.sock"
 listen 4000
-pid File.expand_path('../../tmp/unicorn.pid', __FILE__)
+pid File.expand_path("../tmp/unicorn.pid", __dir__)
 
 old_pid = "#{rails_root}/tmp/unicorn.pid"
-if File.exists?(old_pid)
-  puts "Found existing unicorn PID"
+if File.exist?(old_pid)
   begin
     pid = File.read(old_pid).to_i
     File.delete(old_pid)
     Process.kill("QUIT", pid)
-    puts "shut down previous unicorn (#{pid})"
   rescue Errno::ENOENT, Errno::ESRCH
-    puts "was already dead"
   end
 end
-puts "unicorn started"
-before_fork do |server, worker|
+before_fork do |_server, _worker|
   defined?(ActiveRecord::Base) && ActiveRecord::Base.connection.disconnect!
 end
 
-after_fork do |server, worker|
+after_fork do |_server, _worker|
   defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
 end
 
