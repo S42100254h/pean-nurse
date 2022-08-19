6a8dcdf4803e587179d6939a6bcc100dac6f67f1
yuki sawabe <sawabe0v0@gmail.com>
2022-08-19 18:15:11 +0900

    add subnets to stacks

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from "@aws-cdk/core";
 import { Vpc } from "./resource/vpc";
+import { Subnet } from "./resource/subnet";
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -7,5 +8,8 @@ export class InfraStack extends cdk.Stack {
 
     const vpc = new Vpc();
     vpc.createResources(this);
+
+    const subnet = new Subnet(vpc.vpc);
+    subnet.createResources(this);
   }
 }
