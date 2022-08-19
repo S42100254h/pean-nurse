b339ab3baffaf549e965446bcde01513ea89d1eb
yuki sawabe <sawabe0v0@gmail.com>
2022-08-20 07:50:20 +0900

    add routeTable to stacks

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from "@aws-cdk/core";
 import { Vpc } from "./resource/vpc";
 import { Subnet } from "./resource/subnet";
 import { InternetGateway } from "./resource/internetGateway";
+import { RouteTable } from "./resource/routeTable";
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -15,5 +16,8 @@ export class InfraStack extends cdk.Stack {
 
     const internetGateway = new InternetGateway(vpc.vpc);
     internetGateway.createResources(this);
+
+    const routeTable = new RouteTable(vpc.vpc, subnet.public1a, subnet.public1c, internetGateway.igw);
+    routeTable.createResources(this);
   }
 }
