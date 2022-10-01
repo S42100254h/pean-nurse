f1dd0b355b386bf72d6c907eadc47a79f8ff37f1
yuki sawabe <sawabe0v0@gmail.com>
2022-10-01 14:22:50 +0900

    modify badge type

--- front/src/types/entity/badge.ts
+++ front/src/types/entity/badge.ts
@@ -2,7 +2,12 @@ export type Badge = {
   id: number;
   index: number;
   color: "gold" | "silver" | "bronze";
+  image: Image | null;
   category_id: number;
   created_at: string;
   updated_at: string;
+};
+
+type Image = {
+  url: string;
 };
